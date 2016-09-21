769bb7efb79a7edb6b64ddfd243f5397edc89c72
vector-of-bool <vectorofbool@gmail.com>
2016-09-20 23:14:09 -0600

    `install` target should be parallelizable (Fix #31)

--- src/cmake.ts
+++ src/cmake.ts
@@ -937,7 +937,7 @@ export class CMakeTools {
         // Pass arguments based on a particular generator
         const gen = self.activeGenerator;
         const generator_args = (() => {
-            if (/(Unix|MinGW) Makefiles|Ninja/.test(gen) && target !== 'clean' && target !== 'install')
+            if (/(Unix|MinGW) Makefiles|Ninja/.test(gen) && target !== 'clean')
                 return ['-j', self.numJobs.toString()];
             else if (/Visual Studio/.test(gen))
                 return ['/m', '/property:GenerateFullPaths=true'];
