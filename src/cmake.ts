6e9424984f386fb3ad447fffa9ef8fe4e2a7fc68
vector-of-bool <vectorofbool@gmail.com>
2016-10-04 22:36:36 -0600

    Use refactored GCC parsing code for diags (#38)

--- src/cmake.ts
+++ src/cmake.ts
@@ -768,31 +768,26 @@ export class CMakeTools {
      *      message could be decoded.
      */
     public parseGCCDiagnostic(line: string): Maybe<FileDiagnostic> {
-        const gcc_re = /^(.*):(\d+):(\d+):\s+(warning|error|note):\s+(.*)$/;
-        const res = gcc_re.exec(line);
-        if (!res)
+        const diag = diagnostics.parseGCCDiagnostic(line);
+        if (!diag) {
             return null;
-        const file = res[1];
-        const lineno = Number.parseInt(res[2]) - 1;
-        const column = Number.parseInt(res[3]) - 1;
-        const severity = res[4];
-        const message = res[5];
-        const abspath = path.isAbsolute(file)
-            ? file
-            : path.normalize(path.join(this.binaryDir, file));
-        const diag = new vscode.Diagnostic(
-            new vscode.Range(lineno, column, lineno, column),
-            message,
+        }
+        const abspath = path.isAbsolute(diag.file)
+            ? diag.file
+            : path.normalize(path.join(this.binaryDir, diag.file));
+        const vsdiag = new vscode.Diagnostic(
+            new vscode.Range(diag.line, diag.column, diag.line, diag.column),
+            diag.message,
             {
                 error: vscode.DiagnosticSeverity.Error,
                 warning: vscode.DiagnosticSeverity.Warning,
                 note: vscode.DiagnosticSeverity.Information,
-            }[severity]
+            }[diag.severity]
         );
-        diag.source = 'GCC';
+        vsdiag.source = 'GCC';
         return {
             filepath: abspath,
-            diag: diag,
+            diag: vsdiag,
         };
     }
 
