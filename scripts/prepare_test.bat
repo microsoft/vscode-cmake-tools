44451d0556d53a246d260423e9a021f9f79df82d
KoeMai <github@mairbaeurl.de>
2018-01-16 07:53:19 +0100

    Refactor scripts to build test helper
    - Replace start folder backup by pushd/popd
    - Add comments

--- scripts/prepare_test.bat
+++ scripts/prepare_test.bat
@@ -1,14 +1,20 @@
+pushd .
+REM Location of the fake bin directory where the compiler fakes should be placed
+set PATH_TO_FAKEBIN=..\..\fakebin
+
+REM Build Fake Compilers
 cd test\fakeOutputGenerator
 mkdir build
 cd build
 cmake ..
 cmake --build .
 
-mkdir ..\..\fakebin
-copy ..\configfiles\* ..\..\fakebin
+REM Create fake bin directory with configured compilers
+mkdir %PATH_TO_FAKEBIN%
+copy ..\configfiles\* %PATH_TO_FAKEBIN%
 
-copy Debug\FakeOutputGenerator.exe ..\..\fakebin\clang-0.25.exe
-copy Debug\FakeOutputGenerator.exe ..\..\fakebin\gcc-42.1.exe
-copy Debug\FakeOutputGenerator.exe ..\..\fakebin\gcc-666.exe
+copy Debug\FakeOutputGenerator.exe %PATH_TO_FAKEBIN%\clang-0.25.exe
+copy Debug\FakeOutputGenerator.exe %PATH_TO_FAKEBIN%\gcc-42.1.exe
+copy Debug\FakeOutputGenerator.exe %PATH_TO_FAKEBIN%\gcc-666.exe
 
-cd ..\..\..
+popd
--- scripts/prepare_test.sh
+++ scripts/prepare_test.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
+pushd .
 
-current_dir=$(pwd)
+# Location of the fake bin directory where the compiler fakes should be placed
+PATH_TO_FAKEBIN=../../fakebin
+
 cd test/fakeOutputGenerator
 
+# Build Fake Compilers
 mkdir build
 cd build
 cmake ..
 cmake --build .
 
-mkdir ../../fakebin
-cp ../configfiles/* ../../fakebin
+# Create fake bin directory with configured compilers
+mkdir $PATH_TO_FAKEBIN
+cp ../configfiles/* $PATH_TO_FAKEBIN
 
-cp FakeOutputGenerator ../../fakebin/clang-0.25
-cp FakeOutputGenerator ../../fakebin/gcc-666
-cp FakeOutputGenerator ../../fakebin/gcc-42.1
+cp FakeOutputGenerator $PATH_TO_FAKEBIN/clang-0.25
+cp FakeOutputGenerator $PATH_TO_FAKEBIN/gcc-666
+cp FakeOutputGenerator $PATH_TO_FAKEBIN/gcc-42.1
 
-cd $current_dir
+popd
