0f6449a66520315e6a253ac7e207bb8a4bc24d70
KoeMai <github@mairbaeurl.de>
2018-03-04 16:52:23 +0100

    Restructure test environment for configure and build tests
    The tests for configure and build are ready to use on windows,
    linux and MAC does not work. I extracted all test environment
    parts into separate classes. This allows to reuse them
    (splitting into separate files need to be done later).

--- test/extension_tests/successful_build/test/configure_and_build.test.ts
+++ test/extension_tests/successful_build/test/configure_and_build.test.ts
@@ -12,55 +12,108 @@ import {CMakeTools} from '../../../../src/cmake-tools';
 import {fs} from '../../../../src/pr';
 import {normalizePath} from '../../../../src/util';
 
+class BuildDirectory {
 
-suite('Build', async () => {
-  let cmt: CMakeTools;
-  let sandbox: sinon.SinonSandbox;
+  private location: string;
 
-  suiteSetup(async () => {
-    clearExistingKitConfigurationFile();
-    // clean build folder
-    sandbox = sinon.sandbox.create();
+  private locationOfThisClassFile: string = __dirname;
 
-    sandbox.stub(vscode.window, "showQuickPick").callsFake(function(items: string[]): Thenable<string|undefined> {
-      return Promise.resolve(items[1]);  // How do we make it plattform independent?
-    });
-  });
-  suiteTeardown(async () => {
-    sandbox.restore();
-    await cmt.stop();
+  private getProjectRootDirectory(): string {
+    return path.normalize(
+        path.join(this.locationOfThisClassFile, '../../../../../test/extension_tests/successful_build/project_folder'));
+  }
 
-  })
+  public constructor(relative_location_to_root: string = 'build') {
+    this.location = path.join(this.getProjectRootDirectory(), relative_location_to_root);
+  }
 
-  test('Configure ', async () => {
-    cmt = await getExtension();
-    await cmt.scanForKits();
-    await cmt.selectKit();
+  public async Clear() {
+    if (await fs.exists(this.location)) {
+      fs.rmdir(this.location);
+    }
+  }
 
-    expect(await cmt.configure()).to.be.eq(0);
-  }).timeout(100000);
+  public get Location(): string { return this.location; }
 
-  test.only('Build', async () => {
-    clearExistingKitConfigurationFile();
-    cmt = await getExtension();
-    await cmt.scanForKits();
-    await cmt.selectKit();
+  public get IsCMakeCachePresent(): Promise<boolean> { return fs.exists(path.join(this.Location, "CMakeCache.txt")); }
+}
 
-    expect(await cmt.build()).to.be.eq(0);
-    // How do we get the path to the executable or the output of executeable?
+class TestProgramResult {
 
-    // Get path for output.txt file and check if it exists
-    const file = normalizePath(path.join(await cmt.binaryDir, 'output.txt'));
-    expect(await fs.exists(file)).to.eq(true);
+  private result_file_location: string;
 
-    const content = await fs.readFile(file);
+  public constructor(location: string, filename: string = 'output.txt') {
+    this.result_file_location = normalizePath(path.join(location, filename));
+  }
+
+  public get IsPresent(): Promise<boolean> { return fs.exists(this.result_file_location); }
+
+  public async GetResultAsJson(): Promise<any> {
+    expect(await this.IsPresent).to.eq(true, 'Test programm result file was not found');
+    const content = await fs.readFile(this.result_file_location);
     expect(content.toLocaleString()).to.not.eq('');
 
-    /*
-    // In my case it was the following, but it is environment specific.
-    const jsonContent = JSON.parse(content.toString());
-    expect(jsonContent['compiler']).to.eq('Microsoft Visual Studio');
-    expect(jsonContent['cmake-version']).to.eq('3.9');
-    //*/
-  }).timeout(100000);
+    return JSON.parse(content.toString());
+  }
+}
+
+class DefaultEnvironment {
+
+  sandbox: sinon.SinonSandbox;
+  buildDir: BuildDirectory;
+  result: TestProgramResult;
+
+  public constructor(build_location: string = 'build', executableResult: string = 'output.txt') {
+    this.buildDir = new BuildDirectory(build_location);
+    this.result = new TestProgramResult(this.buildDir.Location, executableResult);
+
+    // clean build folder
+    this.sandbox = sinon.sandbox.create();
+    this.sandbox.stub(vscode.window, 'showQuickPick').callsFake(function(items: string[]): Thenable<string|undefined> {
+      return Promise.resolve(items[1]);  // How do we make it plattform independent?
+    });
+  }
+
+  public teardown(): void { this.sandbox.restore(); }
+}
+
+(process.env.HasVs == 'true' ? suite : suite.skip)('Build', async() => {
+  let cmt: CMakeTools;
+  let testEnv: DefaultEnvironment;
+
+  suiteSetup(async function(this: Mocha.IHookCallbackContext) {
+    this.timeout(10000);
+    testEnv = new DefaultEnvironment();
+    cmt = await getExtension();
+
+    // This test will use all on the same kit.
+    // No rescan of the tools is needed
+    // No new kit selection is needed
+    clearExistingKitConfigurationFile();
+    await cmt.scanForKits();
+    await cmt.selectKit();
+  });
+  suiteTeardown(async function(this: Mocha.IHookCallbackContext) {
+    this.timeout(10000);
+    testEnv.teardown();
+    await cmt.stop();
+  })
+
+  setup(async() => { testEnv.buildDir.Clear(); });
+
+  teardown(async() => { await cmt.stop(); });
+
+  test('Configure ', async() => {
+    expect(await cmt.configure()).to.be.eq(0);
+
+    expect(await testEnv.buildDir.IsCMakeCachePresent).to.be.true;
+  }).timeout(60000);
+
+  test('Build', async() => {
+    expect(await cmt.build()).to.be.eq(0);
+
+    const result = await testEnv.result.GetResultAsJson();
+    expect(result['compiler']).to.eq('Microsoft Visual Studio');
+    expect(result['cmake-version']).to.eq('3.9');
+  }).timeout(60000);
 });
