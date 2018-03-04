4fa9a11359b0814a6df46ff1369cd7b698f2e0f6
Randshot <randshot@norealm.xyz>
2018-03-04 02:09:25 +0100

    Separate test calls

--- scripts/run_extension_tests.bat
+++ scripts/run_extension_tests.bat
@@ -12,18 +12,28 @@ set CODE_TESTS_WORKSPACE=%~dp0\..\test\extension_tests\without_cmakelist_file\pr
 node ./node_modules/vscode/bin/test
 set TEST_ERRONO=%errorlevel%
 
-set CODE_TESTS_PATH=%~dp0\..\out\test\extension_tests\successful_build
-set CODE_TESTS_WORKSPACE=%~dp0\..\test\extension_tests\successful_build\project_folder
-node ./node_modules/vscode/bin/test
-set TEST_ERRONO_2=%errorlevel%
-
 popd
 
 REM forward error level
 if "%TEST_ERRONO%" NEQ "0" (
    exit /b %TEST_ERRONO%
 )
-if "%TEST_ERRONO_2%" NEQ "0" (
+
+pushd
+set HasVs=true
+set CMT_TESTING=1
+
+REM Run all tests with a workspace folder where no CMakeLists.txt exists
+REM This prevents automatic loading of the extension.
+set CODE_TESTS_PATH=%~dp0\..\out\test\extension_tests\successful_build
+set CODE_TESTS_WORKSPACE=%~dp0\..\test\extension_tests\successful_build\project_folder
+node ./node_modules/vscode/bin/test
+set TEST_ERRONO=%errorlevel%
+
+popd
+
+REM forward error level
+if "%TEST_ERRONO%" NEQ "0" (
    exit /b %TEST_ERRONO%
 )
 endlocal
