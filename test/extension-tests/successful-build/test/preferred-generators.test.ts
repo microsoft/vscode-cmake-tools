ff537aa836442e819578f622694db7fb4e9d1588
Randshot <randshot@norealm.xyz>
2018-03-29 00:10:12 +0200

    Add GCC 7.3.0 MingW64 kit
    Signed-off-by: Randshot <randshot@norealm.xyz>

--- test/extension-tests/successful-build/test/preferred-generators.test.ts
+++ test/extension-tests/successful-build/test/preferred-generators.test.ts
@@ -62,6 +62,11 @@ const DEFAULT_CYGWIN_KITS: KitEnvironment[] = [
 
 const DEFAULT_MINGW_KITS: KitEnvironment[] = [
   {
+    defaultKit: 'GCC 7.3.0',
+    expectedDefaultGenerator: 'MinGW Makefiles',
+    path: ['C:\\Program Files\\mingw-w64\\x86_64-7.3.0-posix-seh-rt_v5-rev0\\mingw64\\bin']
+  },
+  {
     defaultKit: 'GCC 7.2.0',
     expectedDefaultGenerator: 'MinGW Makefiles',
     path: ['C:\\Program Files\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\bin']
