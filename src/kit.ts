014abd150ca9ea43e6e85c7043ead88e151473db
Randshot <randshot@norealm.xyz>
2018-03-30 19:07:48 +0200

    Put all vswhere args into one string
    Signed-off-by: Randshot <randshot@norealm.xyz>

--- src/kit.ts
+++ src/kit.ts
@@ -301,18 +301,8 @@ export async function vsInstallations(): Promise<VSInstallation[]> {
   const vswhere_exe = path.join(thisExtensionPath(), 'res/vswhere.exe');
   const sys32_path = path.join(process.env.WINDIR as string, 'System32');
 
-  const vswhere_args = [
-    '/c',
-    `${sys32_path}\\chcp 65001 |`,
-    vswhere_exe,
-    '-all',
-    '-format',
-    'json',
-    '-products',
-    '*',
-    '-legacy',
-    '-prerelease'
-  ];
+  const vswhere_args =
+      ['/c', `${sys32_path}\\chcp 65001 | ${vswhere_exe} -all -format json -products * -legacy -prerelease`];
   const vswhere_res
       = await proc.execute(`${sys32_path}\\cmd.exe`, vswhere_args, null, {silent: true, encoding: 'utf8'}).result;
 
