c669037d124aa4fae42ed3294f69b878078ce61d
KoeMai <github@mairbaeurl.de>
2018-04-22 20:32:12 +0200

    Prepare project for backend unit tests without vscode
    At the moment all tests run inside of vscode. For the backend tests it is not
    necessary. This commit prepares the test environment to execute non
    vscode unit tests.
    
    - Add example test
    - Add to launcher
    - Add to ci scripts

---
+++ test/backend-unit-tests/cmake/cmake-executeable.test.ts
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+
+suite('cmake-executeable', () => {
+  test('Teste', () => {
+    expect(true).to.be.true;
+  });
+});
