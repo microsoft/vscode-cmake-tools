b9c1e2efa0751e34b705afe4906aa65e0a0d25fc
vector-of-bool <vectorofbool@gmail.com>
2018-05-15 22:35:33 -0600

    Fix preferred generator tests

--- test/extension-tests/successful-build/test/preferred-generators.test.ts
+++ test/extension-tests/successful-build/test/preferred-generators.test.ts
@@ -1,10 +1,10 @@
 import {CMakeTools} from '@cmt/cmake-tools';
 import {Kit, scanForKits} from '@cmt/kit';
-import {clearExistingKitConfigurationFile, DefaultEnvironment, expect, getFirstSystemKit} from '@test/util';
+import {clearExistingKitConfigurationFile, DefaultEnvironment, expect} from '@test/util';
 import {ITestCallbackContext} from 'mocha';
 
 interface KitEnvironment {
-  defaultKit: string;
+  kitNamePrefix: string;
   excludeKit?: string;
   expectedDefaultGenerator: string;
   path?: string[];
@@ -23,39 +23,39 @@ if (workername === undefined) {
 const DEFAULT_VS_KITS: KitEnvironment[] = [
   // Visual Studio 2017
   {
-    defaultKit: 'Visual Studio Community 2017',
+    kitNamePrefix: 'Visual Studio Community 2017',
     excludeKit: 'Preview',
     expectedDefaultGenerator: 'Visual Studio 15 2017'
   },
-  {defaultKit: 'Visual Studio Community 2017 Preview', expectedDefaultGenerator: 'Visual Studio 15 2017'},
+  {kitNamePrefix: 'Visual Studio Community 2017 Preview', expectedDefaultGenerator: 'Visual Studio 15 2017'},
   {
-    defaultKit: 'Visual Studio Professional 2017',
+    kitNamePrefix: 'Visual Studio Professional 2017',
     excludeKit: 'Preview',
     expectedDefaultGenerator: 'Visual Studio 15 2017'
   },
-  {defaultKit: 'Visual Studio Professional 2017 Preview', expectedDefaultGenerator: 'Visual Studio 15 2017'},
+  {kitNamePrefix: 'Visual Studio Professional 2017 Preview', expectedDefaultGenerator: 'Visual Studio 15 2017'},
   {
-    defaultKit: 'Visual Studio Enterprise 2017',
+    kitNamePrefix: 'Visual Studio Enterprise 2017',
     excludeKit: 'Preview',
     expectedDefaultGenerator: 'Visual Studio 15 2017'
   },
-  {defaultKit: 'Visual Studio Enterprise 2017 Preview', expectedDefaultGenerator: 'Visual Studio 15 2017'},
+  {kitNamePrefix: 'Visual Studio Enterprise 2017 Preview', expectedDefaultGenerator: 'Visual Studio 15 2017'},
 
   // Visual Studio 2015
-  {defaultKit: 'VisualStudio.14.0', expectedDefaultGenerator: 'Visual Studio 14 2015', path: ['']},
+  {kitNamePrefix: 'VisualStudio.14.0', expectedDefaultGenerator: 'Visual Studio 14 2015', path: ['']},
 
   // Visual Studio 2012
-  {defaultKit: 'VisualStudio.11.0', expectedDefaultGenerator: 'Visual Studio 11 2012', path: ['']},
+  {kitNamePrefix: 'VisualStudio.11.0', expectedDefaultGenerator: 'Visual Studio 11 2012', path: ['']},
 ];
 
 const DEFAULT_CYGWIN_KITS: KitEnvironment[] = [
-  {defaultKit: 'GCC 6.4.0', expectedDefaultGenerator: 'Unix Makefiles', path: ['c:\\cygwin64\\bin']},
-  {defaultKit: 'Clang 4.0.1', expectedDefaultGenerator: 'Unix Makefiles', path: ['c:\\cygwin64\\bin']}
+  {kitNamePrefix: 'GCC 6.4.0', expectedDefaultGenerator: 'Unix Makefiles', path: ['c:\\cygwin64\\bin']},
+  {kitNamePrefix: 'Clang 4.0.1', expectedDefaultGenerator: 'Unix Makefiles', path: ['c:\\cygwin64\\bin']}
 ];
 
 const DEFAULT_MINGW_KITS: KitEnvironment[] = [
   {
-    defaultKit: 'GCC 7.3.0',
+    kitNamePrefix: 'GCC 7.3.0',
     expectedDefaultGenerator: 'MinGW Makefiles',
     path: [
       'C:\\Program Files\\mingw-w64\\x86_64-7.3.0-posix-seh-rt_v5-rev0\\mingw64\\bin',
@@ -63,7 +63,7 @@ const DEFAULT_MINGW_KITS: KitEnvironment[] = [
     ]
   },
   {
-    defaultKit: 'GCC 7.2.0',
+    kitNamePrefix: 'GCC 7.2.0',
     expectedDefaultGenerator: 'MinGW Makefiles',
     path: [
       'C:\\Program Files\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\bin',
@@ -71,21 +71,21 @@ const DEFAULT_MINGW_KITS: KitEnvironment[] = [
     ]
   },
   {
-    defaultKit: 'GCC 6.3.0',
+    kitNamePrefix: 'GCC 6.3.0',
     expectedDefaultGenerator: 'MinGW Makefiles',
     path: [
       'C:\\Program Files\\mingw-w64\\x86_64-6.3.0-posix-seh-rt_v5-rev1\\mingw64\\bin',
       'C:\\mingw-w64\\x86_64-6.3.0-posix-seh-rt_v5-rev1\\mingw64\\bin'
     ]
   },
-  {defaultKit: 'GCC 5.3.0', expectedDefaultGenerator: 'MinGW Makefiles', path: ['C:\\MinGW\\bin']}
+  {kitNamePrefix: 'GCC 5.3.0', expectedDefaultGenerator: 'MinGW Makefiles', path: ['C:\\MinGW\\bin']}
 ];
 
 const DEFAULT_WINDOWS_KITS: KitEnvironment[] = DEFAULT_VS_KITS.concat(DEFAULT_CYGWIN_KITS, DEFAULT_MINGW_KITS);
 
 const KITS_BY_PLATFORM: {[osName: string]: KitEnvironment[]} = {
   ['win32']: DEFAULT_WINDOWS_KITS.concat([{
-    defaultKit: 'Clang 5.0.1',
+    kitNamePrefix: 'Clang 5.0.1',
     expectedDefaultGenerator: 'Unix Makefiles',
     path: [' C:\\Program Files\\LLVM\\bin'],
   }]),
@@ -93,16 +93,16 @@ const KITS_BY_PLATFORM: {[osName: string]: KitEnvironment[]} = {
   ['Visual Studio 2017 Preview']: DEFAULT_WINDOWS_KITS,
   ['Visual Studio 2015']: DEFAULT_WINDOWS_KITS,
   ['linux']: [
-    {defaultKit: 'Clang', expectedDefaultGenerator: 'Unix Makefiles'},
-    {defaultKit: 'GCC', expectedDefaultGenerator: 'Unix Makefiles'}
+    {kitNamePrefix: 'Clang', expectedDefaultGenerator: 'Unix Makefiles'},
+    {kitNamePrefix: 'GCC', expectedDefaultGenerator: 'Unix Makefiles'}
   ],
   ['darwin']: [
-    {defaultKit: 'Clang', expectedDefaultGenerator: 'Unix Makefiles'},
-    {defaultKit: 'GCC', expectedDefaultGenerator: 'Unix Makefiles'}
+    {kitNamePrefix: 'Clang', expectedDefaultGenerator: 'Unix Makefiles'},
+    {kitNamePrefix: 'GCC', expectedDefaultGenerator: 'Unix Makefiles'}
   ],
   // This is a special case for travis
   ['osx']: [{
-    defaultKit: 'Clang',
+    kitNamePrefix: 'Clang',
     expectedDefaultGenerator: 'Unix Makefiles',
     path: [
       '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin',
@@ -134,9 +134,9 @@ function fuzzyKitCheck(kitName: string, defaultKit: string, excludeKit?: string)
 // results, a fuzzy check.
 function isKitAvailable(context: CMakeContext): boolean {
   const kits = context.kits;
-  return kits.find(kit => exactKitCheck(kit.name, context.buildSystem.defaultKit))
+  return kits.find(kit => exactKitCheck(kit.name, context.buildSystem.kitNamePrefix))
       ? true
-      : kits.find(kit => fuzzyKitCheck(kit.name, context.buildSystem.defaultKit, context.buildSystem.excludeKit))
+      : kits.find(kit => fuzzyKitCheck(kit.name, context.buildSystem.kitNamePrefix, context.buildSystem.excludeKit))
           ? true
           : false;
 }
@@ -145,10 +145,10 @@ function isKitAvailable(context: CMakeContext): boolean {
 // defined in the kits file.
 function isPreferredGeneratorAvailable(context: CMakeContext): boolean {
   const kits = context.kits;
-  return kits.find(kit => exactKitCheck(kit.name, context.buildSystem.defaultKit) && kit.preferredGenerator ? true
+  return kits.find(kit => exactKitCheck(kit.name, context.buildSystem.kitNamePrefix) && kit.preferredGenerator ? true
                                                                                                             : false)
       ? true
-      : kits.find(kit => fuzzyKitCheck(kit.name, context.buildSystem.defaultKit, context.buildSystem.excludeKit)
+      : kits.find(kit => fuzzyKitCheck(kit.name, context.buildSystem.kitNamePrefix, context.buildSystem.excludeKit)
                           && kit.preferredGenerator
                       ? true
                       : false)
@@ -183,7 +183,7 @@ function makeExtensionTestSuite(name: string,
       context.testEnv = new DefaultEnvironment('test/extension-tests/successful-build/project-folder',
                                                'build',
                                                'output.txt',
-                                               context.buildSystem.defaultKit,
+                                               context.buildSystem.kitNamePrefix,
                                                context.buildSystem.excludeKit);
 
       context.pathBackup = process.env.PATH!;
@@ -203,6 +203,10 @@ function makeExtensionTestSuite(name: string,
     setup(async function(this: Mocha.IBeforeAndAfterContext) {
       this.timeout(10000);
       context.cmt = await CMakeTools.create(context.testEnv.vsContext, context.testEnv.wsContext);
+      const kit = context.kits.find(k => k.name.startsWith(expectedBuildSystem.kitNamePrefix));
+      // tslint:disable-next-line:no-unused-expression
+      expect(kit, `Kit required for test "${expectedBuildSystem.kitNamePrefix}" is not available.`).to.not.be.null;
+      await context.cmt.setKit(kit!);
       context.testEnv.projectFolder.buildDirectory.clear();
     });
 
@@ -226,16 +230,15 @@ function makeExtensionTestSuite(name: string,
 
 // Preferred generator selection order is settings.json -> cmake-kit.json -> error
 KITS_BY_PLATFORM[workername].forEach(buildSystem => {
-  makeExtensionTestSuite(`Preferred generators (${buildSystem.defaultKit})`, buildSystem, (context: CMakeContext) => {
+  makeExtensionTestSuite(`Preferred generators (${buildSystem.kitNamePrefix})`, buildSystem, (context: CMakeContext) => {
     const BUILD_TIMEOUT: number = 120000;
 
     // This test is only valid for kits which have at least one preferred generator defined.
-    test(`Use preferred generator from kit file (${buildSystem.defaultKit})`,
+    test(`Use preferred generator from kit file (${buildSystem.kitNamePrefix})`,
          async function(this: ITestCallbackContext) {
            skipTestIf({preferredGeneratorIsNotAvailable: true}, this, context);
            this.timeout(BUILD_TIMEOUT);
 
-           await context.cmt.setKit(await getFirstSystemKit());
            context.testEnv.config.updatePartial({preferredGenerators: []});
            expect(await context.cmt.build()).to.eql(0);
            const result = await context.testEnv.result.getResultAsJson();
@@ -243,11 +246,10 @@ KITS_BY_PLATFORM[workername].forEach(buildSystem => {
            expect(context.testEnv.errorMessagesQueue.length).to.eql(0);
          });
 
-    test(`Use preferred generator from settings file (${buildSystem.defaultKit})`,
+    test(`Use preferred generator from settings file (${buildSystem.kitNamePrefix})`,
          async function(this: ITestCallbackContext) {
            this.timeout(BUILD_TIMEOUT);
 
-           await context.cmt.setKit(await getFirstSystemKit());
            context.testEnv.config.updatePartial({
              preferredGenerators: [
                'NMake Makefiles',
@@ -259,7 +261,7 @@ KITS_BY_PLATFORM[workername].forEach(buildSystem => {
            const result = await context.testEnv.result.getResultAsJson();
 
            expect(result['cmake-generator'])
-               .to.eql(context.buildSystem.defaultKit.includes('Visual Studio')
+               .to.eql(context.buildSystem.kitNamePrefix.includes('Visual Studio')
                            ? 'NMake Makefiles'
                            : context.buildSystem.expectedDefaultGenerator);
 
@@ -268,12 +270,11 @@ KITS_BY_PLATFORM[workername].forEach(buildSystem => {
 
     // This test is NOT valid for kits which have any preferred generator defined
     // since we expect CMT to reject the build.
-    test(`Reject invalid preferred generator in settings file (${buildSystem.defaultKit})`,
+    test(`Reject invalid preferred generator in settings file (${buildSystem.kitNamePrefix})`,
          async function(this: ITestCallbackContext) {
            skipTestIf({preferredGeneratorIsAvailable: true}, this, context);
            this.timeout(BUILD_TIMEOUT);
 
-           await context.cmt.setKit(await getFirstSystemKit());
            context.testEnv.config.updatePartial({preferredGenerators: ['BlaBla']});
            await expect(context.cmt.build()).to.eventually.be.rejected;
 
@@ -284,12 +285,11 @@ KITS_BY_PLATFORM[workername].forEach(buildSystem => {
 
     // This test is NOT valid for kits which have any preferred generator defined
     // since we expect CMT to reject the build.
-    test(`Reject if all \'preferredGenerators\' fields are empty (${buildSystem.defaultKit})`,
+    test(`Reject if all \'preferredGenerators\' fields are empty (${buildSystem.kitNamePrefix})`,
          async function(this: ITestCallbackContext) {
            skipTestIf({preferredGeneratorIsAvailable: true}, this, context);
            this.timeout(BUILD_TIMEOUT);
 
-           await context.cmt.setKit(await getFirstSystemKit());
            context.testEnv.config.updatePartial({preferredGenerators: []});
            await expect(context.cmt.build()).to.eventually.be.rejected;
 
@@ -298,11 +298,10 @@ KITS_BY_PLATFORM[workername].forEach(buildSystem => {
                .to.be.contains('Unable to determine what CMake generator to use.');
          });
 
-    test(`Use preferred generator from settings or kit file (${buildSystem.defaultKit})`,
+    test(`Use preferred generator from settings or kit file (${buildSystem.kitNamePrefix})`,
          async function(this: ITestCallbackContext) {
            this.timeout(BUILD_TIMEOUT);
 
-           await context.cmt.setKit(await getFirstSystemKit());
            context.testEnv.config.updatePartial({preferredGenerators: ['Unix Makefiles', 'MinGW Makefiles']});
            expect(await context.cmt.build()).to.eql(0);
            const result = await context.testEnv.result.getResultAsJson();
