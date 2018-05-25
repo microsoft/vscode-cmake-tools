d1941b320a1e97b44438ecf803398c2ea10a29da
KoeMai <github@mairbaeurl.de>
2018-05-25 23:58:05 +0200

    Prepare tests for CI.

--- test/extension-tests/successful-build/test/configure-and-build.test.ts
+++ test/extension-tests/successful-build/test/configure-and-build.test.ts
@@ -1,8 +1,20 @@
 import {CMakeTools} from '@cmt/cmake-tools';
 import {TestProgramResult} from '@test/helpers/testprogram/test-program-result';
-import {DefaultEnvironment, expect} from '@test/util';
+import {DefaultEnvironment, expect, clearExistingKitConfigurationFile} from '@test/util';
+import { ITestCallbackContext } from 'mocha';
 
-suite('Build', async () => {
+
+let workername: string = process.platform;
+
+if (process.env.APPVEYOR_BUILD_WORKER_IMAGE) {
+  workername = process.env.APPVEYOR_BUILD_WORKER_IMAGE;
+}
+
+if (process.env.TRAVIS_OS_NAME) {
+  workername = process.env.TRAVIS_OS_NAME;
+}
+
+suite.only('Build', async () => {
   let cmt: CMakeTools;
   let testEnv: DefaultEnvironment;
 
@@ -18,9 +30,9 @@ suite('Build', async () => {
     // This test will use all on the same kit.
     // No rescan of the tools is needed
     // No new kit selection is needed
-    //await clearExistingKitConfigurationFile();
-    //await cmt.scanForKits();
-    //await cmt.selectKit();
+    await clearExistingKitConfigurationFile();
+    await cmt.scanForKits();
+    await cmt.selectKit();
 
     testEnv.projectFolder.buildDirectory.clear();
   });
@@ -68,165 +80,148 @@ suite('Build', async () => {
     expect(result['cookie']).to.eq('passed-cookie');
   }).timeout(100000);
 
-  test('Test kit switch after missing preferred generator', async () => {
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
+  test('Test kit switch after missing preferred generator', async function(this: ITestCallbackContext) {
+    // Select compiler build node dependent
+    const os_compilers : {[osName: string]: { kitLabel:string, compiler:string}[]} = { ['linux']: [
+      {kitLabel: 'GCC 4.8.4', compiler:'GNU GCC/G++' },
+      {kitLabel: 'Clang 5.0.0', compiler:'Clang/LLVM' }],
+      ['win32']: [
+      {kitLabel: 'GCC 4.8.1', compiler:'GNU GCC/G++' },
+      {kitLabel: 'VisualStudio', compiler:'Microsoft Visual Studio' }
+    ]};
+    if( !(workername in os_compilers)) this.skip();
+    const compiler = os_compilers[workername];
+
+    // Run test
+    testEnv.kitSelection.defaultKitLabel=compiler[0].kitLabel;
     await cmt.selectKit();
 
     await cmt.build();
 
-    testEnv.kitSelection.defaultKitLabel="VisualStudio";
-    await cmt.selectKit();
-
-    await cmt.build();
-    const result1 = await testEnv.result.getResultAsJson();
-    expect(result1['compiler']).to.eql('Microsoft Visual Studio');
-  }).timeout(100000);
-
-  test('Test kit switch between different preferrred generators and compilers', async () => { // Failed
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
-    await cmt.selectKit();
-
-    await cmt.build(); // Test invalid build target (like all on visual studio project<)
-    //const result = await testEnv.result.getResultAsJson();
-
-    //expect(result['compiler']).to.eql('GNU GCC/G++');
-
-    testEnv.kitSelection.defaultKitLabel="VisualStudio";
+    testEnv.kitSelection.defaultKitLabel=compiler[1].kitLabel;
     await cmt.selectKit();
 
     await cmt.build();
     const result1 = await testEnv.result.getResultAsJson();
-    expect(result1['compiler']).to.eql('Microsoft Visual Studio');
-
-    // The test did not clean up the build directory so that the vs compiler is used
+    expect(result1['compiler']).to.eql(compiler[1].compiler);
   }).timeout(100000);
 
-  test('Test kit switch between different preferrred generators and same compiler', async () => { // Failed
+  test('Test kit switch between different preferrred generators and compilers', async function(this: ITestCallbackContext) {
+    // Select compiler build node dependent
+    const os_compilers : {[osName: string]: { kitLabel:string, compiler:string}[]} = { ['linux']: [
+      {kitLabel: 'GCC 4.8.4', compiler:'GNU GCC/G++' },
+      {kitLabel: 'Clang 5.0.0', compiler:'Clang/LLVM' }],
+      ['win32']: [
+      {kitLabel: 'GCC 4.8.1', compiler:'GNU GCC/G++' },
+      {kitLabel: 'VisualStudio', compiler:'Microsoft Visual Studio' }
+    ]};
+    if( !(workername in os_compilers)) this.skip();
+    const compiler = os_compilers[workername];
+
+    testEnv.kitSelection.defaultKitLabel=compiler[0].kitLabel;
+    await cmt.selectKit();
+    await cmt.build();
+
+
+    testEnv.kitSelection.defaultKitLabel=compiler[1].kitLabel;
+    await cmt.selectKit();
+    await cmt.build();
+
+    const result1 = await testEnv.result.getResultAsJson();
+    expect(result1['compiler']).to.eql(compiler[1].compiler);
+  }).timeout(100000);
+
+  test('Test kit switch between different preferrred generators and same compiler', async function(this: ITestCallbackContext) {
+    // Select compiler build node dependent
+    const os_compilers : {[osName: string]: { kitLabel:string, generator: string}[]} = { ['linux']: [
+      {kitLabel: 'Generator switch test GCC Make', generator: 'Unix Makefiles' },
+      {kitLabel: 'Generator switch test GCC Ninja', generator: 'Ninja' }],
+      ['win32']: [
+      {kitLabel: 'Generator switch test GCC Mingw - Win', generator: 'MinGW Makefiles' },
+      {kitLabel: 'Generator switch test GCC Ninja - Win', generator: 'Ninja' }
+    ]};
+    if( !(workername in os_compilers)) this.skip();
+    const compiler = os_compilers[workername];
+
     testEnv.config.updatePartial({preferredGenerators: []});
-
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
-    await cmt.selectKit();
-
-    await cmt.build(); // Test invalid build target (like all on visual studio project<)
-    //const result = await testEnv.result.getResultAsJson();
-
-    //expect(result['compiler']).to.eql('GNU GCC/G++');
-
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1 - Ninja";
+    testEnv.kitSelection.defaultKitLabel=compiler[0].kitLabel;
     await cmt.selectKit();
 
     await cmt.build();
-    //const result1 = await testEnv.result.getResultAsJson();
-    //expect(result1['compiler']).to.eql('Microsoft Visual Studio');
 
-    // The test did not clean up the build directory so that the vs compiler is used
-  }).timeout(100000);
-
-  test('Test kit switch kits after configure', async () => { // Failed
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
+    testEnv.kitSelection.defaultKitLabel=compiler[1].kitLabel;
     await cmt.selectKit();
 
-    await cmt.build(); // Test invalid build target (like all on visual studio project<)
-    //const result = await testEnv.result.getResultAsJson();
+    await cmt.build();
+    const result1 = await testEnv.result.getResultAsJson();
+    expect(result1['cmake-generator']).to.eql(compiler[1].generator);
+  }).timeout(100000);
 
-    //expect(result['compiler']).to.eql('GNU GCC/G++');
+  test('Test kit switch kits after configure', async function(this: ITestCallbackContext) {
+    // Select compiler build node dependent
+    const os_compilers : {[osName: string]: { kitLabel:string, generator: string}[]} = { ['linux']: [
+      {kitLabel: 'Generator switch test GCC Make', generator: 'Unix Makefiles' },
+      {kitLabel: 'Generator switch test GCC Ninja', generator: 'Ninja' }],
+      ['win32']: [
+      {kitLabel: 'Generator switch test GCC Mingw - Win', generator: 'MinGW Makefiles' },
+      {kitLabel: 'Generator switch test GCC Ninja - Win', generator: 'Ninja' }
+    ]};
+    if( !(workername in os_compilers)) this.skip();
+    const compiler = os_compilers[workername];
 
-    testEnv.kitSelection.defaultKitLabel="VisualStudio";
+    testEnv.kitSelection.defaultKitLabel=compiler[0].kitLabel;
+    await cmt.selectKit();
+
+    await cmt.build();
+
+    testEnv.kitSelection.defaultKitLabel=compiler[1].kitLabel;
     await cmt.selectKit();
     await cmt.configure();
 
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
+    testEnv.kitSelection.defaultKitLabel=compiler[0].kitLabel;
     await cmt.selectKit();
 
     await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('GNU GCC/G++');
+    const result1 = await testEnv.result.getResultAsJson();
+    expect(result1['compiler']).to.eql(compiler[0].kitLabel);
   }).timeout(100000);
 
-  test('Test build twice', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
+  test('Test build twice', async function(this: ITestCallbackContext) {
     await cmt.selectKit();
 
     await cmt.build();
     await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('GNU GCC/G++');
+    await testEnv.result.getResultAsJson();
   }).timeout(100000);
 
-  test('Test build twice with clean', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
+  test('Test build twice with clean', async function(this: ITestCallbackContext) {
     await cmt.selectKit();
 
     await cmt.build();
     await cmt.clean();
     await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('GNU GCC/G++');
+
+    await testEnv.result.getResultAsJson();
   }).timeout(100000);
 
-  test('Test build twice with clean configure', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
+  test('Test build twice with clean configure', async function(this: ITestCallbackContext) {
     await cmt.selectKit();
 
     await cmt.build();
     await cmt.cleanConfigure();
     await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('GNU GCC/G++');
+
+    await testEnv.result.getResultAsJson();
   }).timeout(100000);
 
-  test('Test build twice with rebuild configure', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="GCC 4.8.1";
+  test('Test build twice with rebuild configure', async function(this: ITestCallbackContext) {
+    // Select compiler build node dependent
     await cmt.selectKit();
 
     await cmt.build();
     await cmt.cleanRebuild();
     await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('GNU GCC/G++');
-  }).timeout(100000);
 
-
-  test('Test build twice', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="VisualStudio";
-    await cmt.selectKit();
-
-    await cmt.build();
-    await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('Microsoft Visual Studio');
-  }).timeout(100000);
-
-  test('Test build twice with clean', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="VisualStudio";
-    await cmt.selectKit();
-
-    await cmt.build();
-    await cmt.clean();
-    await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('Microsoft Visual Studio'');
-  }).timeout(100000);
-
-  test('Test build twice with clean configure', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="VisualStudio";
-    await cmt.selectKit();
-
-    await cmt.build();
-    await cmt.cleanConfigure();
-    await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('Microsoft Visual Studio');
-  }).timeout(100000);
-
-  test('Test build twice with rebuild configure', async () => { // OK
-    testEnv.kitSelection.defaultKitLabel="VisualStudio";
-    await cmt.selectKit();
-
-    await cmt.build();
-    await cmt.cleanRebuild();
-    await cmt.build();
-    // const result1 = await testEnv.result.getResultAsJson();
-    // expect(result1['compiler']).to.eql('Microsoft Visual Studio');
+    await testEnv.result.getResultAsJson();
   }).timeout(100000);
 });
