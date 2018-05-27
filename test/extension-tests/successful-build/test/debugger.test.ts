bc047bf312d80ed70ac3611b56486e859d7210df
KoeMai <github@mairbaeurl.de>
2018-05-27 14:42:09 +0200

    Add test to check working build on launch

--- test/extension-tests/successful-build/test/debugger.test.ts
+++ test/extension-tests/successful-build/test/debugger.test.ts
@@ -1,6 +1,7 @@
 import {CMakeTools} from '@cmt/cmake-tools';
 import {DefaultEnvironment, expect} from '@test/util';
 import sinon = require('sinon');
+import * as fs from 'fs';
 
 // tslint:disable:no-unused-expression
 
@@ -43,4 +44,23 @@ suite('[Debug/Lauch interface]', async () => {
 
     expect(await cmt.launchTargetPath()).to.be.eq(executablesTargets[0].path);
   });
+
+  test.only('Test build on launch', async () => {
+        const executablesTargets = await cmt.executableTargets;
+        expect(executablesTargets.length).to.be.not.eq(0);
+        await cmt.setLaunchTargetByName(executablesTargets[0].name);
+
+        const launchProgrammPath = await cmt.launchTargetPath();
+        expect(launchProgrammPath).to.be.not.null;
+        const validPath: string = launchProgrammPath!;
+
+        // Check that the compiled files does not exist
+        fs.unlinkSync(validPath);
+        expect(fs.existsSync(validPath)).to.be.false;
+
+        await cmt.launchTargetPath();
+
+        // Check that it is compiled as a new file
+        expect(fs.existsSync(validPath)).to.be.true;
+      }).timeout(60000);
 });
