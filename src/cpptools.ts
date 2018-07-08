c9e466c6919c67eda12d5e61a51b66abd4d313c8
vector-of-bool <vectorofbool@gmail.com>
2018-07-07 20:55:50 -0600

    Parse compile flags to get language standard version and extra compiler definitions

--- src/cpptools.ts
+++ src/cpptools.ts
@@ -7,11 +7,79 @@
 
 import {CMakeCache} from '@cmt/cache';
 import * as cms from '@cmt/cms-client';
+import {createLogger} from '@cmt/logging';
+import rollbar from '@cmt/rollbar';
 import * as util from '@cmt/util';
 import * as path from 'path';
+import * as shlex from 'shlex';
 import * as vscode from 'vscode';
 import * as cpt from 'vscode-cpptools';
-import rollbar from './rollbar';
+
+const log = createLogger('cpptools');
+
+type StandardVersion = 'c89'|'c99'|'c11'|'c++98'|'c++03'|'c++11'|'c++14'|'c++17';
+
+export interface CompileFlagInformation {
+  extraDefinitions: string[];
+  standard: StandardVersion;
+}
+
+export function parseCompileFlags(args: string[]): CompileFlagInformation {
+  const iter = args[Symbol.iterator]();
+  const extraDefinitions: string[] = [];
+  let standard: StandardVersion = 'c++17';
+  while (1) {
+    const {done, value} = iter.next();
+    if (done) {
+      break;
+    }
+    const lower = value.toLowerCase();
+    if (value === '-D' || value === '/D') {
+      // tslint:disable-next-line:no-shadowed-variable
+      const {done, value} = iter.next();
+      if (done) {
+        rollbar.error('Unexpected end of parsing command line arguments');
+        continue;
+      }
+      extraDefinitions.push(value);
+    } else if (value.startsWith('-D') || value.startsWith('/D')) {
+      const def = value.substring(2);
+      extraDefinitions.push(def);
+    } else if (value.startsWith('-std=') || lower.startsWith('-std:') || lower.startsWith('/std:')) {
+      const std = value.substring(5);
+      if (std.endsWith('++14') || std.endsWith('++1y')) {
+        standard = 'c++14';
+      } else if (std.endsWith('++17') || std.endsWith('++1z')) {
+        standard = 'c++17';
+      } else if (std.endsWith('++11') || std.endsWith('++0x')) {
+        standard = 'c++11';
+      } else if (std.endsWith('++2a')) {
+        // Not yet supported...
+      } else if (std.endsWith('++98')) {
+        standard = 'c++98';
+      } else if (std.endsWith('++03')) {
+        standard = 'c++03';
+      } else {
+        // GNU options from: https://gcc.gnu.org/onlinedocs/gcc/C-Dialect-Options.html#C-Dialect-Options
+        if (/(c|gnu)(90|89|iso9899:(1990|199409))/.test(value)) {
+          standard = 'c89';
+        } else if (/(c|gnu)(99|9x|iso9899:(1999|199x))/.test(value)) {
+          standard = 'c99';
+        } else if (/(c|gnu)(11|1x|iso9899:2011)/.test(value)) {
+          standard = 'c11';
+        } else if (/(c|gnu)(17|18|iso9899:(2017|2018))/.test(value)) {
+          // Not supported by cpptools
+          // standardVersion = 'c17';
+          standard = 'c11';
+        } else {
+          log.warning('Unknown standard control flag: ', value);
+          standard = 'c++17';
+        }
+      }
+    }
+  }
+  return {extraDefinitions, standard};
+}
 
 /**
  * Type given when updating the configuration data stored in the file index.
@@ -78,26 +146,30 @@ export class CppConfigurationProvider implements cpt.CustomConfigurationProvider
 
   /**
    * Create a source file configuration for the given file group.
-   * @param grp The file group from the code model to create config data for
+   * @param fileGroup The file group from the code model to create config data for
    * @param opts Index update options
    */
-  private _buildConfigurationData(grp: cms.CodeModelFileGroup, opts: CodeModelParams): cpt.SourceFileConfiguration {
+  private _buildConfigurationData(fileGroup: cms.CodeModelFileGroup,
+                                  opts: CodeModelParams): cpt.SourceFileConfiguration {
     // If the file didn't have a language, default to C++
-    const lang = grp.language || 'CXX';
+    const lang = fileGroup.language || 'CXX';
     // Try the group's language's compiler, then the C++ compiler, then the C compiler.
     const comp_cache = opts.cache.get(`CMAKE_${lang}_COMPILER`) || opts.cache.get('CMAKE_CXX_COMPILER')
         || opts.cache.get('CMAKE_C_COMPILER');
     // Try to get the path to the compiler we want to use
     const comp_path = comp_cache ? comp_cache.as<string>() : opts.clCompilerPath;
     if (!comp_path) {
-      rollbar.error('Unable to automatically determine compiler', {lang, fileGroup: grp});
+      rollbar.error('Unable to automatically determine compiler', {lang, fileGroup});
     }
     const is_msvc = comp_path && (path.basename(comp_path).toLocaleLowerCase() === 'cl.exe');
+    const flags = shlex.split(fileGroup.compileFlags);
+    const {standard, extraDefinitions} = parseCompileFlags(flags);
+    const defines = (fileGroup.defines || []).concat(extraDefinitions);
     return {
-      defines: grp.defines || [],
-      includePath: (grp.includePath || []).map(p => p.path),
+      defines,
+      standard,
+      includePath: (fileGroup.includePath || []).map(p => p.path),
       intelliSenseMode: is_msvc ? 'msvc-x64' : 'clang-x64',
-      standard: 'c++17',  // TODO: Switch on correct standard
       compilerPath: comp_path || undefined,
     };
   }
---
+++ test/unit-tests/cpptools.test.ts
@@ -0,0 +1,18 @@
+import {parseCompileFlags} from '@cmt/cpptools';
+import {expect} from '@test/util';
+
+// tslint:disable:no-unused-expression
+
+suite.only('CppTools tests', () => {
+  test('Parse some compiler flags', () => {
+    let info = parseCompileFlags(['-DFOO=BAR']);
+    expect(info.extraDefinitions).to.eql(['FOO=BAR']);
+    info = parseCompileFlags(['-D', 'FOO=BAR']);
+    expect(info.extraDefinitions).to.eql(['FOO=BAR']);
+    info = parseCompileFlags(['-DFOO=BAR', '/D', 'BAZ=QUX']);
+    expect(info.extraDefinitions).to.eql(['FOO=BAR', 'BAZ=QUX']);
+    expect(info.standard).to.eql('c++17');
+    info = parseCompileFlags(['-std=c++03']);
+    expect(info.standard).to.eql('c++03');
+  });
+});
