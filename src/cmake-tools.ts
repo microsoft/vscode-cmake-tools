e796798d82f6b0c9172cde3c35f267f831143285
KoeMai <github@mairbaeurl.de>
2018-12-22 15:37:43 +0100

    Fix bug #601 Problem with CMake:Execute current target without debugger
    Commandline shellPath with slash does not work, it needs a backslash.

--- src/cmake-tools.ts
+++ src/cmake-tools.ts
@@ -1086,7 +1086,7 @@ export class CMakeTools implements vscode.Disposable, api.CMakeToolsAPI {
     };
     if (process.platform == 'win32') {
       // Use cmd.exe on Windows
-      termOptions.shellPath = 'C:/Windows/System32/cmd.exe';
+      termOptions.shellPath = 'C:\\Windows\\System32\\cmd.exe';
     }
     if (!this._launchTerminal)
       this._launchTerminal = vscode.window.createTerminal(termOptions);
