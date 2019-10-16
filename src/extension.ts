1f172c339b1cadc53d6c6dcb7231d59926851ae3
Bob Brown <bobbrow@users.noreply.github.com>
2019-10-16 15:49:21 -0700

    debugTarget keybinding conflicts with vscode default (#833)

--- src/extension.ts
+++ src/extension.ts
@@ -1244,6 +1244,7 @@ class SchemaProvider implements vscode.TextDocumentContentProvider {
 export async function activate(context: vscode.ExtensionContext) {
   // Register a protocol handler to serve localized schemas
   vscode.workspace.registerTextDocumentContentProvider('cmake-tools-schema', new SchemaProvider());
+  vscode.commands.executeCommand("setContext", "inCMakeProject", true);
 
   await vscode.window.withProgress(
       {
