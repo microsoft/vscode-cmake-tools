915daea0f80412d83776f1fafce09bf1be5585ef
Andreea Isac <andris@microsoft.com>
2019-11-11 08:20:04 -0800

    Return undefined explicitly when OS is not windows

--- src/kit.ts
+++ src/kit.ts
@@ -725,20 +725,23 @@ export async function scanForClangCLKits(searchPaths: string[]): Promise<Promise
 }
 
 async function getVSInstallForKit(kit: Kit): Promise<VSInstallation|undefined> {
-    if (process.platform === "win32") {
-        console.assert(kit.visualStudio);
-        console.assert(kit.visualStudioArchitecture);
-        const installs = await vsInstallations();
-        const match = (inst: VSInstallation) =>
-            // old Kit format
-            (legacyKitVSName(inst) == kit.visualStudio) ||
-            // new Kit format
-            (kitVSName(inst) === kit.visualStudio) ||
-            // Clang for VS kit format
-            (!!kit.compilers && kit.name.indexOf("Clang") >= 0 && kit.name.indexOf(vsDisplayName(inst)) >= 0);
-
-        return installs.find(inst => match(inst));
+    if (process.platform !== "win32") {
+        return undefined;
     }
+
+    console.assert(kit.visualStudio);
+    console.assert(kit.visualStudioArchitecture);
+
+    const installs = await vsInstallations();
+    const match = (inst: VSInstallation) =>
+        // old Kit format
+        (legacyKitVSName(inst) == kit.visualStudio) ||
+        // new Kit format
+        (kitVSName(inst) === kit.visualStudio) ||
+        // Clang for VS kit format
+        (!!kit.compilers && kit.name.indexOf("Clang") >= 0 && kit.name.indexOf(vsDisplayName(inst)) >= 0);
+
+    return installs.find(inst => match(inst));
 }
 
 export async function getVSKitEnvironment(kit: Kit): Promise<Map<string, string>|null> {
