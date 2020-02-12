0e8bf1360f9c144ef307d236f7e62fe3fba980bc
Zingam <zingam@outlook.com>
2020-02-12 02:01:54 +0200

    Disabled TSLint warnings on FSWatcher.close() (#1022)

--- src/kitsController.ts
+++ src/kitsController.ts
@@ -62,6 +62,7 @@ export class KitsController {
     if (this._pickKitCancellationTokenSource) {
       this._pickKitCancellationTokenSource.dispose();
     }
+    // tslint:disable-next-line: no-floating-promises
     this._kitsWatcher.close();
   }
 
