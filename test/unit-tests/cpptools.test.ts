c7e367c652a6e6d8623abcca5ba7552e71e5f81c
Sean McManus <seanmcm@microsoft.com>
2020-04-06 13:31:15 -0700

    Fix tests.

--- test/unit-tests/cpptools.test.ts
+++ test/unit-tests/cpptools.test.ts
@@ -4,10 +4,11 @@ import { CMakeCache } from '@cmt/cache';
 import * as path from 'path';
 import * as codemodel_api from '@cmt/drivers/codemodel-driver-interface';
 import * as vscode from 'vscode';
+import * as util from '@cmt/util';
 
 // tslint:disable:no-unused-expression
 
-const here = __dirname;
+const here = util.lightNormalizePath(__dirname);
 function getTestResourceFilePath(filename: string): string {
   return path.normalize(path.join(here, '../../../test/unit-tests', filename));
 }
@@ -62,30 +63,9 @@ suite('CppTools tests', () => {
     };
 
     provider.updateConfigurationData({cache, codeModel, activeTarget: 'target1', folder: here});
-    let configurations = await provider.provideConfigurations([uri]);
-    expect(configurations.length).to.eq(1);
-    expect(configurations[0].configuration.defines).to.contain('FLAG1');
 
-    provider.updateConfigurationData({cache, codeModel, activeTarget: 'target2', folder: here});
-    configurations = await provider.provideConfigurations([uri]);
-    expect(configurations.length).to.eq(1);
-    expect(configurations[0].configuration.defines).to.contain('FLAG2');
-
-    provider.updateConfigurationData({cache, codeModel, activeTarget: 'all', folder: here});
-    configurations = await provider.provideConfigurations([uri]);
-    expect(configurations.length).to.eq(1);
-    expect(configurations[0].configuration.defines.some(def => def === 'FLAG1' || def === 'FLAG2')).to.be.true;
-    expect(configurations[0].configuration.defines).to.not.have.all.members(['FLAG1', 'FLAG2']);
-
-    // Verify the per-folder browsePath.
-    let canProvideBrowseConfigPerFolder: boolean = await provider.canProvideBrowseConfigurationsPerFolder();
-    expect(canProvideBrowseConfigPerFolder).to.eq(true);
-    let browseConfig = await provider.provideFolderBrowseConfiguration(vscode.Uri.file(path.join(here, __filename)));
-    expect(browseConfig.browsePath.length).to.eq(1);
-    expect(browseConfig.browsePath[0]).to.eq(here);
-
-    // Verify the browsePath with a different folder.
-    let smokeFolder = path.join(here, '../smoke');
+    // Update configuration with a 2nd workspace folder.
+    let smokeFolder = util.lightNormalizePath(path.join(here, '../smoke'));
     const sourceFile2 = path.join(smokeFolder, 'main.cpp');
     const uri2 = vscode.Uri.file(sourceFile2);
     const codeModel2: codemodel_api.CodeModelContent = {
@@ -108,10 +88,34 @@ suite('CppTools tests', () => {
       }]
     };
     provider.updateConfigurationData({cache, codeModel: codeModel2, activeTarget: 'target1', folder: smokeFolder});
+
+    let configurations = await provider.provideConfigurations([uri]);
+    expect(configurations.length).to.eq(1);
+    expect(configurations[0].configuration.defines).to.contain('FLAG1');
+
+    provider.updateConfigurationData({cache, codeModel, activeTarget: 'target2', folder: here});
+    configurations = await provider.provideConfigurations([uri]);
+    expect(configurations.length).to.eq(1);
+    expect(configurations[0].configuration.defines).to.contain('FLAG2');
+
+    provider.updateConfigurationData({cache, codeModel, activeTarget: 'all', folder: here});
+    configurations = await provider.provideConfigurations([uri]);
+    expect(configurations.length).to.eq(1);
+    expect(configurations[0].configuration.defines.some(def => def === 'FLAG1' || def === 'FLAG2')).to.be.true;
+    expect(configurations[0].configuration.defines).to.not.have.all.members(['FLAG1', 'FLAG2']);
+
+    // Verify the per-folder browsePath.
+    let canProvideBrowseConfigPerFolder: boolean = await provider.canProvideBrowseConfigurationsPerFolder();
+    expect(canProvideBrowseConfigPerFolder).to.eq(true);
+    let browseConfig = await provider.provideFolderBrowseConfiguration(vscode.Uri.file(here));
+    expect(browseConfig.browsePath.length).to.eq(1);
+    expect(browseConfig.browsePath[0]).to.eq(here);
+
+    // Verify the browsePath with a different folder.
     let configurations2 = await provider.provideConfigurations([uri2]);
     expect(configurations2.length).to.eq(1);
     expect(configurations2[0].configuration.defines).to.contain('FLAG1');
-    let browseConfig2 = await provider.provideFolderBrowseConfiguration(vscode.Uri.file(path.join(smokeFolder, "smoke.ts")));
+    let browseConfig2 = await provider.provideFolderBrowseConfiguration(vscode.Uri.file(smokeFolder));
     expect(browseConfig2.browsePath.length).to.eq(1);
     expect(browseConfig2.browsePath[0]).to.eq(smokeFolder);
   });
