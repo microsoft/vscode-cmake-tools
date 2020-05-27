7eee395faab1827a6cd708a324c20d2a7bb71c3d
Vitaly Poterba <vptrbv@gmail.com>
2020-05-27 21:55:54 +0300

    Reverted another problem fix

--- src/extension.ts
+++ src/extension.ts
@@ -1112,6 +1112,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
   // Register a protocol handler to serve localized schemas
   vscode.workspace.registerTextDocumentContentProvider('cmake-tools-schema', new SchemaProvider());
+  vscode.commands.executeCommand("setContext", "inCMakeProject", true);
 
   await vscode.window.withProgress(
       {
