00625fd18eefdba841c9c38a6e85c66b058c8af1
alan-wr <68377984+alan-wr@users.noreply.github.com>
2020-10-08 18:22:17 +0800

    Problems with CMake: Quick Start #1004 (#1419)
    Signed-off-by: alan-wr <aijun.shi@windriver.com>
    
    Co-authored-by: Andreea Isac <48239328+andreeis@users.noreply.github.com>

--- src/cmake-tools.ts
+++ src/cmake-tools.ts
@@ -1391,7 +1391,7 @@ export class CMakeTools implements vscode.Disposable, api.CMakeToolsAPI {
       return -2;
     }
 
-    const sourceDir = cmtFolder.folder.uri.fsPath;
+    const sourceDir = await this.sourceDir;
     const mainListFile = path.join(sourceDir, 'CMakeLists.txt');
 
     if (await fs.exists(mainListFile)) {
