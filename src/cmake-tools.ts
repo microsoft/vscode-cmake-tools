e68cee2b85d97b7a3de0b560ca29e30025369ec1
estshorter <1430311+estshorter@users.noreply.github.com>
2020-10-08 19:08:17 +0900

    Preserve focus when executing "CMake: Run Without Debugging" (#1452)
    Co-authored-by: Andreea Isac <48239328+andreeis@users.noreply.github.com>

--- src/cmake-tools.ts
+++ src/cmake-tools.ts
@@ -1378,7 +1378,7 @@ export class CMakeTools implements vscode.Disposable, api.CMakeToolsAPI {
       this._launchTerminal = vscode.window.createTerminal(termOptions);
     const quoted = shlex.quote(executable.path);
     this._launchTerminal.sendText(quoted);
-    this._launchTerminal.show();
+    this._launchTerminal.show(true);
     return this._launchTerminal;
   }
 
