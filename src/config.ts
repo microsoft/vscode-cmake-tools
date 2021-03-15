cd1ab1382d5d857dd6912f74808a0c43d63ba225
Elaheh Rashedi <elrashed@microsoft.com>
2021-03-15 14:28:16 -0700

    remove import

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,6 @@ import * as os from 'os';
 import * as telemetry from '@cmt/telemetry';
 import * as vscode from 'vscode';
 import * as nls from 'vscode-nls';
-import { replace } from 'sinon';
 
 nls.config({messageFormat: nls.MessageFormat.bundle, bundleFormat: nls.BundleFormat.standalone})();
 const localize: nls.LocalizeFunc = nls.loadMessageBundle();
