f46159556a35dcc9a0d54b29caa1d5a2aa2b6802
elrashed <elahehrashedi@gmail.com>
2021-06-09 20:49:43 -0700

    add env variable "HasVs": "true"

--- test/unit-tests/runTest.ts
+++ test/unit-tests/runTest.ts
@@ -16,7 +16,7 @@ async function main() {
 
     const launchArgs = [ "--disable-extensions", testWorkspace ];
 
-    const extensionTestsEnv: { [key: string]: string | undefined } = { "CMT_TESTING": "1" };
+    const extensionTestsEnv: { [key: string]: string | undefined } = { "CMT_TESTING": "1", "HasVs": "true" };
 
     // Download VS Code, unzip it and run the integration test
     await runTests({ launchArgs, extensionDevelopmentPath, extensionTestsPath, extensionTestsEnv });
