d962894f58ab095f964acb1d4b8363dd2130144a
elrashed <elahehrashedi@gmail.com>
2021-07-16 20:48:07 -0700

    change path pattern

--- test/extension-tests/single-root-UI/index.ts
+++ test/extension-tests/single-root-UI/index.ts
@@ -14,7 +14,7 @@ export function run(): Promise<void> {
   const testsRoot = __dirname;
 
   return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+    glob('**/*.test.js', { cwd: testsRoot }, (err, files) => {
       if (err) {
         return e(err);
       }
