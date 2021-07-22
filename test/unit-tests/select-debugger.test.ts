7a1f299cb3a3fbaf887ceb8f6ad2d3cb6b520c64
Xinyu Sui <xisui@microsoft.com>
2021-07-22 13:00:58 -0700

    Fix new lint errors in tests

--- test/unit-tests/select-debugger.test.ts
+++ test/unit-tests/select-debugger.test.ts
@@ -94,7 +94,7 @@ suite('Select debugger', async () => {
     const target = {name: 'Test', path: 'Target'};
     const cache = await CMakeCache.fromPath(getTestResourceFilePath('TestCMakeCache-gcc.txt'));
 
-    expect(Debugger.getDebugConfigurationFromCache(cache, target, 'linux')).to.be.rejectedWith(Error);
+    await expect(Debugger.getDebugConfigurationFromCache(cache, target, 'linux')).to.be.rejectedWith(Error);
   });
 
   test('Create debug config from cache - GCC 5 fallback test', async () => {
