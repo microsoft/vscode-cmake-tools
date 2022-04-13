c2af3e5e48579bfdac506bd381a0b385ba234c1d
Bob Brown (DEVDIV) <bobbrow@microsoft.com>
2022-04-13 10:01:39 -0700

    Fix the bool test

--- src/drivers/cmakeDriver.ts
+++ src/drivers/cmakeDriver.ts
@@ -809,7 +809,7 @@ export abstract class CMakeDriver implements vscode.Disposable {
                 if (gen.name.toLowerCase().startsWith('xcode') && platform === 'darwin') {
                     return gen;
                 }
-                if (this.cmakeGenerators.indexOf(gen.name)) {
+                if (this.cmakeGenerators.indexOf(gen.name) >= 0) {
                     return gen;
                 }
                 continue;
