f17155221cdf59b2946022dd3b9bebeafbd3d928
Bob Brown (DEVDIV) <bobbrow@microsoft.com>
2022-06-16 10:54:05 -0700

    set the cwd on the file api driver to match the other drivers

--- src/drivers/cmakeFileApiDriver.ts
+++ src/drivers/cmakeFileApiDriver.ts
@@ -261,7 +261,7 @@ export class CMakeFileApiDriver extends CMakeDriver {
             log.debug(`Configuring using ${this.useCMakePresets ? 'preset' : 'kit'}`);
             log.debug('Invoking CMake', cmake, 'with arguments', JSON.stringify(args));
             const env = await this.getConfigureEnvironment();
-            const res = await this.executeCommand(cmake, args, outputConsumer, { environment: env }).result;
+            const res = await this.executeCommand(cmake, args, outputConsumer, { environment: env, cwd: this.binaryDir }).result;
             log.trace(res.stderr);
             log.trace(res.stdout);
             if (res.retc === 0) {
