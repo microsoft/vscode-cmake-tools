10a0bd0091977e36e1803699ea33e74d3ca989d5
Bob Brown <bobbrow@users.noreply.github.com>
2022-06-24 15:21:45 -0700

    Add target name to terminal window name (#2620)

--- test/extension-tests/successful-build/test/debugger.test.ts
+++ test/extension-tests/successful-build/test/debugger.test.ts
@@ -182,7 +182,7 @@ suite('Debug/Launch interface', async () => {
 
         const terminal = await cmt.launchTarget();
         expect(terminal).to.be.not.null;
-        expect(terminal!.name).to.eq('CMake/Launch');
+        expect(terminal!.name).to.eq(`CMake/Launch - ${executablesTargets[0].name}`);
 
         const start = new Date();
         // Needed to get launch target result
@@ -221,7 +221,7 @@ suite('Debug/Launch interface', async () => {
 
         const term2 = await cmt.launchTarget();
         expect(term2).to.be.not.null;
-        expect(term2!.name).of.be.eq('CMake/Launch');
+        expect(term2!.name).to.eq(`CMake/Launch - ${executablesTargets[0].name}`);
 
         const term2Pid = await term2?.processId;
         expect(term1Pid).to.not.eq(term2Pid);
