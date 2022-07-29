c7c5255fdeb458d24c362b8a8de631333b6d6876
elrashed <elahehrashedi@gmail.com>
2022-07-29 14:06:22 -0700

    check stderr in clang

--- src/kit.ts
+++ src/kit.ts
@@ -158,7 +158,7 @@ export async function getCompilerVersion(vendor: CompilerVendorEnum, binPath: st
         pr.report({ message: localize('getting.compiler.version', 'Getting {0} version for {1}', vendor, binPath) });
     }
     const exec = await proc.execute(binPath, ['-v'], undefined, { overrideLocale: true, timeout: 30000 }).result;
-    if (exec.retc !== 0) {
+    if (exec.retc !== 0 && !exec.stderr) {
         log.debug(localize('bad.compiler.binary', 'Bad {0} binary ("-v" returns {1}): {2}', vendor, exec.retc, binPath));
         return null;
     }
