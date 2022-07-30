f7f66e558845cdb3c5ee37f8bb0a751870d907d2
Colen Garoutte-Carson <coleng@microsoft.com>
2022-07-29 20:42:38 -0700

    cleanup

--- src/proc.ts
+++ src/proc.ts
@@ -149,6 +149,7 @@ export function execute(command: string, args?: string[], outputConsumer?: Outpu
             resolve({ retc: 0, stdout: '', stderr: '' });
         });
     } else {
+        // Since we won't be sending anything to this process, close its stdin.
         spawn_opts.stdio = ['ignore', 'pipe', 'pipe'];
         try {
             child = proc.spawn(command, args ?? [], spawn_opts);
@@ -166,161 +167,81 @@ export function execute(command: string, args?: string[], outputConsumer?: Outpu
             };
         }
 
-        // Since we won't be sending anything to this process, close stdin.
-        if (!!child.stdin) {
-            console.log('child.stdin ignored, but still defined');
-            child.stdin.end();
-        }
-
         if (options.encoding) {
             child.stdout?.setEncoding(options.encoding);
-        }
-
-        if (!child.stdout) {
-            console.log('child.stdout undefined');
-        }
-        if (!child.stderr) {
-            console.log('child.stderr undefined');
         }
 
         const encoding = options.outputEncoding && iconv.encodingExists(options.outputEncoding) ? options.outputEncoding : 'utf8';
 
         result = new Promise<ExecutionResult>(resolve => {
+            let resolved = false;
             let stdout_acc = '';
             let line_acc = '';
             let stderr_acc = '';
             let stderr_line_acc = '';
-            let timeoutId: NodeJS.Timeout;
-            let withinStdoutData: boolean = false;
-            let withinStderrData: boolean = false;
-            let startedStdoutData: boolean = false;
-            let startedStderrData: boolean = false;
-            let startedLamba1: boolean = false;
-            let startedLamba2: boolean = false;
+            let timeout: NodeJS.Timeout | undefined;
             if (options?.timeout) {
-                const startTimeout: number = new Date().getTime();
-                timeoutId = setTimeout(() => {
-                    const firedTimeout: number = new Date().getTime();
-
-                    console.log(localize('process.timeout', 'The command timed out: {0}', cmdstr));
-                    console.log(`Timeout fired after: ${firedTimeout - startTimeout}ms`);
-                    // console.log(`environment used:`);
-
-                    // for (const key in final_env) {
-                    //     const value = final_env[key];
-                    //     console.log(`ENV: ${key}: ${value}`);
-                    // }
-
-                    if (startedStderrData) {
-                        console.log('stderr had started before timeout');
+                timeout = setTimeout(() => {
+                    timeout = undefined;
+                    if (!resolved) {
+                        log.warning(localize('process.timeout', 'The command timed out: {0}', `${cmdstr}`));
                     }
-                    if (startedStdoutData) {
-                        console.log('stdout had started before timeout');
+                    if (!resolved) {
+                        resolved = true;
+                        child?.kill("SIGKILL");
+                        resolve({retc: -1, stdout: stdout_acc, stderr: stderr_acc });
                     }
-                    if (withinStderrData) {
-                        console.log('stderr incomplete');
-                    }
-                    if (withinStdoutData) {
-                        console.log('stdout incomplete');
-                    }
-                    if (startedLamba1) {
-                        console.log('lambda 1 ran');
-                    }
-                    if (startedLamba2) {
-                        console.log('lambda 2 ran');
-                    }
-                    child?.kill("SIGKILL");
-                    // resolve({retc: -1, stdout: stdout_acc, stderr: stderr_acc });
                 }, options?.timeout);
             }
-            child?.on('error', err => {
-                console.log(localize('process.error', 'The command threw error: {0}', cmdstr));
-                resolve({ retc: -1, stdout: "", stderr: err.message ?? '' });
-            });
-            child?.on('exit', (code, signal) => {
-                if (code !== 0) {
-                    console.log(localize('process.stopped', 'The command: {0} exited with code: {1} and signal: {2}', cmdstr, code, signal));
-                    console.log(`exit << stdout: ${stdout_acc} , stderr: ${stderr_acc} >>`);
-                }
-                if (options?.timeout) {
-                    clearTimeout(timeoutId);
-                }
-                resolve({retc: code, stdout: stdout_acc, stderr: stderr_acc });
-            });
             child?.stdout?.on('data', (data: Uint8Array) => {
-                withinStdoutData = true;
-                startedStdoutData = true;
-                try {
-                    rollbar.invoke(localize('processing.data.event.stdout', 'Processing "data" event from proc stdout'), { data, command, args }, () => {
-                        startedLamba1 = true;
-                        try {
-                            const str = iconv.decode(Buffer.from(data), encoding);
-                            const lines = str.split('\n').map(l => l.endsWith('\r') ? l.substr(0, l.length - 1) : l);
-                            while (lines.length > 1) {
-                                line_acc += lines[0];
-                                if (outputConsumer) {
-                                    outputConsumer.output(line_acc);
-                                } else if (util.isTestMode()) {
-                                    log.info(line_acc);
-                                }
-                                line_acc = '';
-                                // Erase the first line from the list
-                                lines.splice(0, 1);
-                            }
-                            console.assert(lines.length, 'Invalid lines', JSON.stringify(lines));
-                            line_acc += lines[0];
-                            stdout_acc += str;
-                        } catch (err: any) {
-                            console.log(`caught an exception in rollbar.invoke() lambda 1:  ${err}`);
+                rollbar.invoke(localize('processing.data.event.stdout', 'Processing "data" event from proc stdout'), { data, command, args }, () => {
+                    const str = iconv.decode(Buffer.from(data), encoding);
+                    const lines = str.split('\n').map(l => l.endsWith('\r') ? l.substr(0, l.length - 1) : l);
+                    while (lines.length > 1) {
+                        line_acc += lines[0];
+                        if (outputConsumer) {
+                            outputConsumer.output(line_acc);
+                        } else if (util.isTestMode()) {
+                            log.info(line_acc);
                         }
-                    });
-                } catch (err: any) {
-                    console.log(`2 close stdout rollbar error:  ${err}`);
-                }
-                withinStdoutData = false;
+                        line_acc = '';
+                        // Erase the first line from the list
+                        lines.splice(0, 1);
+                    }
+                    console.assert(lines.length, 'Invalid lines', JSON.stringify(lines));
+                    line_acc += lines[0];
+                    stdout_acc += str;
+                });
             });
             child?.stderr?.on('data', (data: Uint8Array) => {
-                withinStderrData = true;
-                startedStderrData = true;
-                try {
-                    rollbar.invoke(localize('processing.data.event.stderr', 'Processing "data" event from proc stderr'), { data, command, args }, () => {
-                        startedLamba2 = true;
-                        try {
-                            const str = iconv.decode(Buffer.from(data), encoding);
-                            const lines = str.split('\n').map(l => l.endsWith('\r') ? l.substr(0, l.length - 1) : l);
-                            while (lines.length > 1) {
-                                stderr_line_acc += lines[0];
-                                if (outputConsumer) {
-                                    outputConsumer.error(stderr_line_acc);
-                                } else if (util.isTestMode() && stderr_line_acc) {
-                                    log.info(stderr_line_acc);
-                                }
-                                stderr_line_acc = '';
-                                // Erase the first line from the list
-                                lines.splice(0, 1);
-                            }
-                            console.assert(lines.length, 'Invalid lines', JSON.stringify(lines));
-                            stderr_line_acc += lines[0];
-                            stderr_acc += str;
-                        } catch (err: any) {
-                            console.log(`caught an exception in rollbar.invoke() lambda 2: ${err}`);
+                rollbar.invoke(localize('processing.data.event.stderr', 'Processing "data" event from proc stderr'), { data, command, args }, () => {
+                    const str = iconv.decode(Buffer.from(data), encoding);
+                    const lines = str.split('\n').map(l => l.endsWith('\r') ? l.substr(0, l.length - 1) : l);
+                    while (lines.length > 1) {
+                        stderr_line_acc += lines[0];
+                        if (outputConsumer) {
+                            outputConsumer.error(stderr_line_acc);
+                        } else if (util.isTestMode() && stderr_line_acc) {
+                            log.info(stderr_line_acc);
                         }
-                    });
-                } catch (err: any) {
-                    console.log(`2 close stderr rollbar error:  ${err}`);
-                }
-                withinStderrData = false;
+                        stderr_line_acc = '';
+                        // Erase the first line from the list
+                        lines.splice(0, 1);
+                    }
+                    console.assert(lines.length, 'Invalid lines', JSON.stringify(lines));
+                    stderr_line_acc += lines[0];
+                    stderr_acc += str;
+                });
             });
-            // Don't stop until the child stream is closed, otherwise we might not read
-            // the whole output of the command.
+            // The 'close' event is emitted after a process has ended and the stdio streams of a child process have been closed.
+            // This is distinct from the 'exit' event, since multiple processes might share the same stdio streams.
+            // The 'close' event will always emit after 'exit' was already emitted, or 'error' if the child failed to spawn.
             child?.on('close', retc => {
-                if (cmdstr.includes('clang')) {
-                    console.log('close: {0}', `${cmdstr}`);
+                if (!!timeout) {
+                    clearTimeout(timeout);
+                    timeout = undefined;
                 }
                 try {
-                    if (options?.timeout) {
-                        clearTimeout(timeoutId);
-                    }
                     rollbar.invoke(localize('resolving.close.event', 'Resolving process on "close" event'), { line_acc, stderr_line_acc, command, retc }, () => {
                         if (line_acc && outputConsumer) {
                             outputConsumer.output(line_acc);
@@ -330,95 +251,10 @@ export function execute(command: string, args?: string[], outputConsumer?: Outpu
                         }
                         resolve({ retc, stdout: stdout_acc, stderr: stderr_acc });
                     });
-                } catch (err: any) {
-                    console.log(`2 close rollbar error:  ${err}`);
+                } catch (e: any) {
                     resolve({ retc, stdout: stdout_acc, stderr: stderr_acc });
                 }
             });
-
-            // // child?.on('disconnect', () => {
-            // //     if (cmdstr.includes('clang')) {
-            // //         console.log(`disconnect: ${cmdstr}`);
-            // //     }
-            // // });
-            // // child?.on('message', () => {
-            // //     if (cmdstr.includes('clang')) {
-            // //         console.log(`message: ${cmdstr}`);
-            // //     }
-            // // });
-
-            // child?.stderr?.on('close', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stderr close: ${cmdstr}`);
-            //     }
-            // });
-
-            // child?.stderr?.on('end', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stderr end: ${cmdstr}`);
-            //     }
-            // });
-
-            // child?.stderr?.on('error', (err: Error) => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stderr error: ${cmdstr} ${JSON.stringify(err)}`);
-            //     }
-            // });
-
-            // child?.stderr?.on('pause', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stderr pause: ${cmdstr}`);
-            //     }
-            // });
-
-            // child?.stderr?.on('readable', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stderr readable: ${cmdstr}`);
-            //     }
-            // });
-
-            // // child?.stderr?.on('resume', () => {
-            // //     if (cmdstr.includes('clang')) {
-            // //         console.log(`stderr resume: ${cmdstr}`);
-            // //     }
-            // // });
-
-            // child?.stdout?.on('close', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stdout close: ${cmdstr}`);
-            //     }
-            // });
-
-            // child?.stdout?.on('end', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stdout end: ${cmdstr}`);
-            //     }
-            // });
-
-            // child?.stdout?.on('error', (err: Error) => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stderr error: ${cmdstr} ${JSON.stringify(err)}`);
-            //     }
-            // });
-
-            // child?.stdout?.on('pause', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stdout pause: ${cmdstr}`);
-            //     }
-            // });
-
-            // child?.stdout?.on('readable', () => {
-            //     if (cmdstr.includes('clang')) {
-            //         console.log(`stdout readable: ${cmdstr}`);
-            //     }
-            // });
-
-            // // child?.stdout?.on('resume', () => {
-            // //     if (cmdstr.includes('clang')) {
-            // //         console.log(`stdout resume: ${cmdstr}`);
-            // //     }
-            // // });
-
         });
     }
     return { child, result };
