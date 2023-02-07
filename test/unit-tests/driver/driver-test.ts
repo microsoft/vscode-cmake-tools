8a17ca3e28799127079b085ea833aeadda68c4eb
elrashed <elahehrashedi@gmail.com>
2023-02-07 11:02:44 -0800

    update timeout

--- test/unit-tests/driver/driver-test.ts
+++ test/unit-tests/driver/driver-test.ts
@@ -206,7 +206,7 @@ export function makeDriverTestsuite(driverName: string, driver_generator: (cmake
             driver = await driver_generator(executable, config, ninjaKitDefault, emptyWorkspaceFolder);
             expect(await driver.cleanConfigure(ConfigureTrigger.runTests, [])).to.be.eq(-2);
             expect(driver.lastCMakePreconditionError).to.be.eq(CMakePreconditionProblems.MissingCMakeListsFile);
-        }).timeout(60000);
+        }).timeout(90000);
 
         test('No parallel configuration', async () => {
             const config = ConfigurationReader.create();
