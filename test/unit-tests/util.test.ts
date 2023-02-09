ddb98648b409a11153ca46896ffa232ff277e133
Bob Brown (DEVDIV) <bobbrow@microsoft.com>
2023-02-09 11:39:15 -0800

    print the time it takes to complete a build

--- test/unit-tests/util.test.ts
+++ test/unit-tests/util.test.ts
@@ -1,13 +1,30 @@
-import { splitPath } from '@cmt/util';
+import * as util from '@cmt/util';
 import { expect } from '@test/util';
 
 suite('Utils test', () => {
     test('Split path into elements', () => {
-        const elems = splitPath('foo/bar/baz');
+        const elems = util.splitPath('foo/bar/baz');
         expect(elems).to.eql(['foo', 'bar', 'baz']);
     });
     test('Split empty path', () => {
-        const elems = splitPath('');
+        const elems = util.splitPath('');
         expect(elems).to.eql([]);
+    });
+    test('Milliseconds to time span string', () => {
+        const tests: [x: number, s: string][] = [
+            [0, '00:00:00.000'],
+            [10, '00:00:00.010'],
+            [100, '00:00:00.100'],
+            [1000, '00:00:01.000'],
+            [10000, '00:00:10.000'],
+            [60000, '00:01:00.000'],
+            [600000, '00:10:00.000'],
+            [3600000, '01:00:00.000'],
+            [36000000, '10:00:00.000'],
+            [39599999, '10:59:59.999']
+        ];
+        for (const test of tests) {
+            expect(util.msToString(test[0])).to.eq(test[1]);
+        }
     });
 });
