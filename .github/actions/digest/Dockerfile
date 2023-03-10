3f4030c3069fb9d9e6836221dee993d01f171987
Ben McMorran <bemcmorr@microsoft.com>
2023-03-10 11:16:46 -0800

    Create HTML report in container

---
+++ .github/actions/digest/Dockerfile
@@ -0,0 +1,3 @@
+FROM python:3
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ .github/actions/digest/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -l
+
+python -m pip install dinghy
+dinghy https://github.com/microsoft/vscode-cmake-tools
