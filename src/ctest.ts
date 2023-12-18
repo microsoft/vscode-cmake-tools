834c8d7abee5c663e3474517a6e71d028d241a0d
Garrett Campbell <gcampbell@microsoft.com>
2023-12-18 11:34:46 -0500

    clean output before build during tests

--- src/ctest.ts
+++ src/ctest.ts
@@ -14,6 +14,7 @@ import { testArgs, TestPreset } from './preset';
 import { expandString } from './expand';
 import * as proc from '@cmt/proc';
 import { ProjectController } from './projectController';
+import { extensionManager } from './extension';
 
 nls.config({ messageFormat: nls.MessageFormat.bundle, bundleFormat: nls.BundleFormat.standalone })();
 const localize: nls.LocalizeFunc = nls.loadMessageBundle();
@@ -957,6 +958,7 @@ export class CTestDriver implements vscode.Disposable {
                     status = 1;
                 } else {
                     try {
+                        extensionManager?.cleanOutputChannel();
                         const buildResult = await project.build(undefined, false, false);
                         if (buildResult !== 0) {
                             status = 2;
