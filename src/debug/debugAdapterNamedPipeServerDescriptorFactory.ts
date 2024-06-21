225c0a0a3212b375eaec9b72ce11ea2d8794ef9b
Garrett Campbell <gcampbell@microsoft.com>
2024-06-21 15:12:10 -0400

    investigating

--- src/debug/debugAdapterNamedPipeServerDescriptorFactory.ts
+++ src/debug/debugAdapterNamedPipeServerDescriptorFactory.ts
@@ -18,59 +18,59 @@ export class DebugAdapterNamedPipeServerDescriptorFactory implements vscode.Debu
         const pipeName =
             session.configuration.pipeName ?? getDebuggerPipeName();
 
-        // undocumented configuration field that lets us know if the session is being invoked from a command
-        // This should only be used from inside the extension from a command that invokes the debugger.
-        if (!session.configuration.fromCommand) {
-            const debuggerInformation: DebuggerInformation = {
-                pipeName,
-                dapLog: session.configuration.dapLog,
-                debuggerIsReady: () => undefined
-            };
+        const debuggerInformation: DebuggerInformation = {
+            pipeName,
+            dapLog: session.configuration.dapLog,
+            debuggerIsReady: () => undefined
+        };
 
-            const cmakeDebugType: "configure" | "script" | "external" = session.configuration.cmakeDebugType;
-            if (cmakeDebugType === "configure" || cmakeDebugType === "script") {
-                const promise = new Promise<void>((resolve) => {
-                    debuggerInformation.debuggerIsReady = resolve;
-                });
+        const cmakeDebugType: "configure" | "script" | "external" = session.configuration.cmakeDebugType;
+        if (cmakeDebugType === "configure" || cmakeDebugType === "script") {
+            const promise = new Promise<void>((resolve) => {
+                debuggerInformation.debuggerIsReady = resolve;
+            });
 
-                if (cmakeDebugType === "script") {
-                    const script = session.configuration.scriptPath;
-                    if (!fs.existsSync(script)) {
-                        throw new Error(localize("cmake.debug.scriptPath.does.not.exist", "The script path, \"{0}\", could not be found.", script));
-                    }
-                    const args: string[] = session.configuration.scriptArgs ?? [];
-                    const env = new Map<string, string>(session.configuration.scriptEnv?.map((e: {name: string; value: string}) => [e.name, e.value])) ?? new Map();
-                    logCMakeDebuggerTelemetry(originatedFromLaunchConfiguration, cmakeDebugType);
-                    void executeScriptWithDebugger(script, args, env, debuggerInformation);
-                } else {
-                    logCMakeDebuggerTelemetry(originatedFromLaunchConfiguration, cmakeDebugType);
-                    if (session.configuration.clean) {
-                        if (session.configuration.configureAll) {
-                            void extensionManager?.cleanConfigureAllWithDebuggerInternal(
-                                debuggerInformation
-                            );
-                        } else {
-                            void extensionManager?.cleanConfigureWithDebuggerInternal(
-                                debuggerInformation
-                            );
-                        }
+            if (cmakeDebugType === "script") {
+                const script = session.configuration.scriptPath;
+                if (!fs.existsSync(script)) {
+                    throw new Error(localize("cmake.debug.scriptPath.does.not.exist", "The script path, \"{0}\", could not be found.", script));
+                }
+                const args: string[] = session.configuration.scriptArgs ?? [];
+                const env = new Map<string, string>(session.configuration.scriptEnv?.map((e: {name: string; value: string}) => [e.name, e.value])) ?? new Map();
+                logCMakeDebuggerTelemetry(originatedFromLaunchConfiguration, cmakeDebugType);
+                void executeScriptWithDebugger(script, args, env, debuggerInformation);
+            } else {
+                logCMakeDebuggerTelemetry(originatedFromLaunchConfiguration, cmakeDebugType);
+                if (session.configuration.clean) {
+                    if (session.configuration.configureAll) {
+                        void extensionManager?.cleanConfigureAllWithDebuggerInternal(
+                            debuggerInformation,
+                            session.configuration.trigger
+                        );
                     } else {
-                        if (session.configuration.configureAll) {
-                            void extensionManager?.configureAllWithDebuggerInternal(
-                                debuggerInformation
-                            );
-                        } else {
-                            void extensionManager?.configureWithDebuggerInternal(
-                                debuggerInformation
-                            );
-                        }
+                        void extensionManager?.cleanConfigureWithDebuggerInternal(
+                            debuggerInformation,
+                            session.configuration.folder
+                        );
+                    }
+                } else {
+                    if (session.configuration.configureAll) {
+                        void extensionManager?.configureAllWithDebuggerInternal(
+                            debuggerInformation,
+                            session.configuration.trigger
+                        );
+                    } else {
+                        void extensionManager?.configureWithDebuggerInternal(
+                            debuggerInformation,
+                            session.configuration.folder, session.configuration.showCommandOnly, session.configuration.sourceDir, session.configuration.trigger
+                        );
                     }
                 }
-
-                await promise;
-            } else if (cmakeDebugType === "external") {
-                logCMakeDebuggerTelemetry(originatedFromLaunchConfiguration, cmakeDebugType);
             }
+
+            await promise;
+        } else if (cmakeDebugType === "external") {
+            logCMakeDebuggerTelemetry(originatedFromLaunchConfiguration, cmakeDebugType);
         }
 
         logger.info(localize('debugger.create.descriptor', 'Connecting debugger on named pipe: \"{0}\"', pipeName));
--- src/extension.ts
+++ src/extension.ts
@@ -1286,7 +1286,13 @@ export class ExtensionManager implements vscode.Disposable {
     }
 
     cleanConfigureWithDebugger(folder?: vscode.WorkspaceFolder) {
-        return this.cleanConfigureWithDebuggerInternal({pipeName: getDebuggerPipeName()}, folder);
+        return vscode.debug.startDebugging(undefined, {
+            name: localize("cmake.debug.name", "CMake Debugger"),
+            request: "launch",
+            type: "cmake",
+            cmakeDebugType: "configure",
+            pipeName: getDebuggerPipeName()
+        });
     }
 
     cleanConfigureWithDebuggerInternal(debuggerInformation: DebuggerInformation, folder?: vscode.WorkspaceFolder) {
@@ -1300,7 +1306,14 @@ export class ExtensionManager implements vscode.Disposable {
     }
 
     cleanConfigureAllWithDebugger(trigger?: ConfigureTrigger) {
-        return this.cleanConfigureAllWithDebuggerInternal({pipeName: getDebuggerPipeName()}, trigger);
+        return vscode.debug.startDebugging(undefined, {
+            name: localize("cmake.debug.name", "CMake Debugger"),
+            request: "launch",
+            type: "cmake",
+            cmakeDebugType: "configure",
+            pipeName: getDebuggerPipeName(),
+            trigger
+        });
     }
 
     cleanConfigureAllWithDebuggerInternal(debuggerInformation: DebuggerInformation, trigger?: ConfigureTrigger) {
@@ -1315,7 +1328,13 @@ export class ExtensionManager implements vscode.Disposable {
     }
 
     configureWithDebugger(folder?: vscode.WorkspaceFolder, sourceDir?: string, trigger?: ConfigureTrigger) {
-        return this.configureWithDebuggerInternal({pipeName: getDebuggerPipeName()}, folder, undefined, sourceDir, trigger);
+        return vscode.debug.startDebugging(undefined, {
+            name: localize("cmake.debug.name", "CMake Debugger"),
+            request: "launch",
+            type: "cmake",
+            cmakeDebugType: "configure",
+            pipeName: getDebuggerPipeName()
+        });
     }
 
     configureWithDebuggerInternal(debuggerInformation: DebuggerInformation, folder?: vscode.WorkspaceFolder, showCommandOnly?: boolean, sourceDir?: string, trigger?: ConfigureTrigger) {
@@ -1333,7 +1352,13 @@ export class ExtensionManager implements vscode.Disposable {
     }
 
     configureAllWithDebugger(trigger?: ConfigureTrigger) {
-        return this.configureAllWithDebuggerInternal({pipeName: getDebuggerPipeName()}, trigger);
+        return vscode.debug.startDebugging(undefined, {
+            name: localize("cmake.debug.name", "CMake Debugger"),
+            request: "launch",
+            type: "cmake",
+            cmakeDebugType: "configure",
+            pipeName: getDebuggerPipeName()
+        });
     }
 
     configureAllWithDebuggerInternal(debuggerInformation: DebuggerInformation, trigger?: ConfigureTrigger) {
