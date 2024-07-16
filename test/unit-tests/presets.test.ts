bdc9f5f0c45aa46dcc1a805cacf5b45ff665e54b
Garrett Campbell <86264750+gcampbell-msft@users.noreply.github.com>
2024-07-16 15:35:40 -0400

    Add setting to control how we use VS Dev Env (#3892)
    * committing state, no progress, just thinking through
    
    * fix how we apply and override devenv to ensure we don't accidentally delete preset environment modifications from user
    
    * check if ninja exists and it's the generator, use that to determine if we should add vs devenv as well
    
    * currently broken, but saving state for adding a setting
    
    * this works well, but I need to add back code that prohibits repeated devenv searching with where.exe
    
    * finalize implementation of the setting for useVsDeveloperEnvironment
    
    * minimize branches
    
    * fix import
    
    * update comments
    
    * fix comment
    
    * remove unnecessary comment
    
    * fallback to better architecture/toolset
    
    * update devenv vars and tests
    
    * fix test for mac
    
    * fix tests
    
    * update changelog
    
    * fix test expand and where.exe env
    
    * update test to explicitly test architecture unit test
    
    ---------
    
    Co-authored-by: snehara99 <113148726+snehara99@users.noreply.github.com>

--- test/unit-tests/presets.test.ts
+++ test/unit-tests/presets.test.ts
@@ -1,5 +1,6 @@
 import { Condition, evaluateCondition, getArchitecture, getToolset } from '../../src/preset';
 import { expect } from '@test/util';
+import * as os from "os";
 
 suite('Preset tests', () => {
     test('Parse architecture', () => {
@@ -7,7 +8,15 @@ suite('Preset tests', () => {
         expect(getArchitecture({ name: 'test', architecture: 'amd64' })).to.eq('amd64');
         expect(getArchitecture({ name: 'test', architecture: { value: 'arm', strategy: 'set' } })).to.eq('arm');
         expect(getArchitecture({ name: 'test', architecture: { value: 'arm64', strategy: 'external' } })).to.eq('arm64');
-        expect(getArchitecture({ name: 'test' })).to.eq('x86');
+        if (os.arch() === "arm64") {
+            expect(getArchitecture({ name: "test" })).to.eq("arm64");
+        } else if (os.arch() === "arm") {
+            expect(getArchitecture({ name: "test" })).to.eq("arm");
+        } else if (os.arch() === "x32" || os.arch() === "ia32") {
+            expect(getArchitecture({ name: "test" })).to.eq("x86");
+        } else {
+            expect(getArchitecture({ name: "test" })).to.eq("x64");
+        }
         expect(getArchitecture({ name: 'test', architecture: 'bogus' })).to.eq('bogus');
     });
 
