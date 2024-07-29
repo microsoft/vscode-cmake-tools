143dc5540ca0612a0010868598d2b815ad9ae3a1
Garrett Campbell <gcampbell@microsoft.com>
2024-07-29 10:58:26 -0400

    update telemetry key

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -129,7 +129,8 @@ export function logEvent(eventName: string, properties?: Properties, measures?: 
     sendTelemetry();
 }
 
-const appInsightsKey: string = "AIF-d9b70cd4-b9f9-4d70-929b-a071c400b217";
+const appInsightsKey: string =
+    "A0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255";
 function getPackageInfo(): IPackageInfo {
     const packageJSON: util.PackageJSON = util.thisExtensionPackage();
     return {
