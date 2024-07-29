3e3304ca7cbf764c10a082aafa568290a6ab1a8d
Garrett Campbell <gcampbell@microsoft.com>
2024-07-29 11:00:54 -0400

    typo

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -130,7 +130,7 @@ export function logEvent(eventName: string, properties?: Properties, measures?: 
 }
 
 const appInsightsKey: string =
-    "A0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255";
+    "0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255";
 function getPackageInfo(): IPackageInfo {
     const packageJSON: util.PackageJSON = util.thisExtensionPackage();
     return {
