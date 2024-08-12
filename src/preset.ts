a1be30436403b142a394a6119c0bc922ac51abd4
Garrett Campbell <gcampbell@microsoft.com>
2024-08-12 10:16:37 -0400

    add telemetry for developer environment use

--- src/preset.ts
+++ src/preset.ts
@@ -262,6 +262,9 @@ export interface ConfigurePreset extends Preset {
     vendor?: VendorVsSettings | VendorType;
     toolchainFile?: string;
     installDir?: string;
+
+    // Private fields
+    __developerEnvironmentArchitecture?: string; // Private field to indicate which VS Dev Env architecture we're using, if VS Dev Env is used.
 }
 
 export interface InheritsConfigurePreset extends Preset {
@@ -1088,7 +1091,11 @@ async function tryApplyVsDevEnv(preset: ConfigurePreset, workspaceFolder: string
         }
     }
 
-    preset.__parentEnvironment = EnvironmentUtils.mergePreserveNull([process.env, developerEnvironment]);
+    if (developerEnvironment) {
+        preset.__developerEnvironmentArchitecture = getArchitecture(preset);
+    }
+
+    preset.__parentEnvironment = EnvironmentUtils.mergePreserveNull([process.env, preset.__parentEnvironment, developerEnvironment]);
 }
 /**
  * Expands the configure preset variables.
