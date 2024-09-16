3738ea55dcc52d124008dc374887673dff4d4d42
Garrett Campbell <gcampbell@microsoft.com>
2024-09-16 08:33:36 -0400

    try update partial

--- test/unit-tests/driver/cmakeFileApiDriver.test.ts
+++ test/unit-tests/driver/cmakeFileApiDriver.test.ts
@@ -7,6 +7,10 @@ import { makeCodeModelDriverTestsuite } from './driver-codemodel-tests';
 import { makeDriverTestsuite } from './driver-test';
 
 async function cmakeFileApiDriverFactory(cmake: CMakeExecutable, config: ConfigurationReader, kit: Kit | null, workspaceFolder: string, preconditionHandler: CMakePreconditionProblemSolver, preferredGenerators: CMakeGenerator[]) {
+    if (process.platform === "darwin") {
+        config.updatePartial({ generator: "Unix Makefiles" });
+    }
+
     const d: CMakeFileApiDriver = await CMakeFileApiDriver.create(cmake, config, workspaceFolder || "", false, false, kit, null, null, null, null, null, workspaceFolder, preconditionHandler, preferredGenerators);
     return d;
 }
