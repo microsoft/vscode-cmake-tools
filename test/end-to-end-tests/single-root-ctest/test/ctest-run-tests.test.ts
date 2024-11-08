9c1b1f22ea20a57ad6de3607cced7a1e55f0b11b
Guillaume Peillex <guillaume.peillex@cea.fr>
2024-11-08 16:51:29 +0100

    Adds a new end to end test directory devoted to ctest tests

---
+++ test/end-to-end-tests/single-root-ctest/test/ctest-run-tests.test.ts
@@ -0,0 +1,63 @@
+import { fs } from '@cmt/pr';
+import {
+    clearExistingKitConfigurationFile,
+    DefaultEnvironment,
+    expect
+} from '@test/util';
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+suite('Ctest run tests', () => {
+    let testEnv: DefaultEnvironment;
+    let compdb_cp_path: string;
+
+    suiteSetup(async function (this: Mocha.Context) {
+        this.timeout(100000);
+
+        const build_loc = 'build';
+        const exe_res = 'output.txt';
+
+        // CMakePresets.json and CMakeUserPresets.json exist so will use presets by default
+        testEnv = new DefaultEnvironment('test/end-to-end-tests/single-root-ctest/project-folder', build_loc, exe_res);
+        compdb_cp_path = path.join(testEnv.projectFolder.location, 'compdb_cp.json');
+
+        await clearExistingKitConfigurationFile();
+    });
+
+    setup(async function (this: Mocha.Context) {
+        this.timeout(100000);
+
+        await vscode.workspace.getConfiguration('cmake', vscode.workspace.workspaceFolders![0].uri).update('useCMakePresets', 'always');
+        // await vscode.commands.executeCommand('cmake.getSettingsChangePromise');
+
+        await vscode.commands.executeCommand('cmake.setConfigurePreset', 'Linux1');
+        await vscode.commands.executeCommand('cmake.setBuildPreset', '__defaultBuildPreset__');
+        await vscode.commands.executeCommand('cmake.setTestPreset', '__defaultTestPreset__');
+        await vscode.commands.executeCommand('cmake.setPackagePreset', '__defaultPackagePreset__');
+        await vscode.commands.executeCommand('cmake.setWorkflowPreset', '__defaultWorkflowPreset__');
+
+        await vscode.commands.executeCommand('cmake.build');
+
+        // testEnv.projectFolder.buildDirectory.clear();
+    });
+
+    teardown(async function (this: Mocha.Context) {
+    });
+
+    suiteTeardown(async () => {
+        if (testEnv) {
+            testEnv.teardown();
+        }
+        if (await fs.exists(compdb_cp_path)) {
+            await fs.unlink(compdb_cp_path);
+        }
+    });
+
+    test('Test of ctest', async () => {
+        expect(await vscode.commands.executeCommand('cmake.ctest')).to.be.eq(0);
+
+        const result = await testEnv.result.getResultAsJson();
+        expect(result['cookie']).to.eq('passed-cookie');
+    }).timeout(100000);
+
+});
