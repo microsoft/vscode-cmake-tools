aa5d49ee54d7b4d0278bdd12d7fd55bb642962d9
Guillaume Peillex <guillaume.peillex@gmail.com>
2024-11-21 16:25:31 +0100

    Turns method into free function to be reused in other test suites

--- test/end-to-end-tests/single-root-ctest/test/ctest-run-tests.test.ts
+++ test/end-to-end-tests/single-root-ctest/test/ctest-run-tests.test.ts
@@ -6,57 +6,58 @@ import {
 import * as path from 'path';
 import * as vscode from 'vscode';
 import paths from '@cmt/paths';
+import { use } from 'chai';
+
+/**
+ * Given a CMakePresets.json content, this function will return the configure preset with the given name
+ *
+ * @param presets_content: The content of the CMakePresets.json file as a JSON object
+ * @param preset_name: The name of the configure preset to find
+ * @returns The configure preset with the given name or undefined if not found
+ */
+async function getSpecificPreset(presets_content: any, preset_name: string) {
+    expect(presets_content['configurePresets']).to.not.eq('', "Unable to find configurePresets section!");
+    const all_conf_presets = presets_content['configurePresets'];
+    for (let index = 0; index < all_conf_presets.length; index++) {
+        const conf_preset = all_conf_presets[index];
+        expect(conf_preset['name']).to.not.eq('', "Unable to find name of the current configure preset!");
+        if (conf_preset['name'] === preset_name) {
+            return conf_preset;
+        }
+    }
+    return undefined;
+}
+
+/**
+ * This function removes the test result files and the test directory
+ */
+async function cleanUpTestResultFiles(test_env: DefaultEnvironment, configure_preset: string) {
+    const used_preset = await getSpecificPreset(await getCMakePresetsAsJson(test_env), configure_preset);
+    expect(used_preset['cacheVariables']['TESTS_DIR']).to.not.eq('', "Unable to find the TESTS_DIR cache variable in the configure preset!");
+    const test_dir_path = used_preset['cacheVariables']['TESTS_DIR'];
+    expect("/" + test_dir_path.split('/')[1]).to.eq(paths.tmpDir, `WARNING: The TESTS_DIR variable (${test_dir_path}) does not seem to point to the temporary directory (${paths.tmpDir})!`);
+    await fs.rmdir(test_dir_path);
+    const output_test_path: string = path.join(test_env.projectFolder.location, test_env.buildLocation, test_env.executableResult);
+    if (await fs.exists(output_test_path)) {
+        await fs.unlink(output_test_path);
+    }
+}
+
+/**
+ *
+ * @returns The content of the CMakePresets.json file as a JSON object
+ */
+async function getCMakePresetsAsJson(test_env: DefaultEnvironment) {
+    const preset_location: string = path.join(test_env.projectFolder.location, "CMakePresets.json");
+    expect(fs.existsSync(preset_location)).to.eq(true, `CMakePresets.json file ${preset_location} was not found`);
+    const content = await fs.readFile(preset_location);
+    expect(content.toLocaleString()).to.not.eq('');
+    return JSON.parse(content.toString());
+}
 
 suite('Ctest run tests', () => {
     let testEnv: DefaultEnvironment;
     const usedConfigPreset: string = "AllTestsSuccessfull";
-
-    /**
-     *
-     * @returns The content of the CMakePresets.json file as a JSON object
-     */
-    async function getCMakePresetsAsJson() {
-        const preset_location: string = path.join(testEnv.projectFolder.location, "CMakePresets.json");
-        expect(fs.existsSync(preset_location)).to.eq(true, `CMakePresets.json file ${ preset_location } was not found`);
-        const content = await fs.readFile(preset_location);
-        expect(content.toLocaleString()).to.not.eq('');
-        return JSON.parse(content.toString());
-    }
-
-    /**
-     * Given a CMakePresets.json content, this function will return the configure preset with the given name
-     * 
-     * @param presets_content: The content of the CMakePresets.json file as a JSON object
-     * @param preset_name: The name of the configure preset to find
-     * @returns The configure preset with the given name or undefined if not found
-     */
-    async function getSpecificPreset(presets_content: any, preset_name: string) {
-        expect(presets_content['configurePresets']).to.not.eq('', "Unable to find configurePresets section!");
-        const all_conf_presets = presets_content['configurePresets'];
-        for (let index = 0; index < all_conf_presets.length; index++) {
-            const conf_preset = all_conf_presets[index];
-            expect(conf_preset['name']).to.not.eq('', "Unable to find name of the current configure preset!");
-            if (conf_preset['name'] === preset_name) {
-                return conf_preset;
-            }
-        }
-        return undefined;
-    }
-
-    /**
-     * This function removes the test result files and the test directory
-     */
-    async function cleanUpTestResultFiles() {
-        const used_preset = await getSpecificPreset(await getCMakePresetsAsJson(), usedConfigPreset);
-        expect(used_preset['cacheVariables']['TESTS_DIR']).to.not.eq('', "Unable to find the TESTS_DIR cache variable in the configure preset!");
-        const test_dir_path = used_preset['cacheVariables']['TESTS_DIR'];
-        expect("/" + test_dir_path.split('/')[1]).to.eq(paths.tmpDir, `WARNING: The TESTS_DIR variable (${test_dir_path}) does not seem to point to the temporary directory (${paths.tmpDir})!`);
-        await fs.rmdir(test_dir_path);
-        const output_test_path: string = path.join(testEnv.projectFolder.location, testEnv.buildLocation, testEnv.executableResult);
-        if (await fs.exists(output_test_path)) {
-            await fs.unlink(output_test_path);
-        }
-    }
 
     suiteSetup(async function (this: Mocha.Context) {
         this.timeout(100000);
@@ -81,11 +82,11 @@ suite('Ctest run tests', () => {
     });
 
     setup(async function (this: Mocha.Context) {
-        await cleanUpTestResultFiles();
+        await cleanUpTestResultFiles(testEnv, usedConfigPreset);
     });
 
     teardown(async function (this: Mocha.Context) {
-        await cleanUpTestResultFiles();
+        await cleanUpTestResultFiles(testEnv, usedConfigPreset);
     });
 
     suiteTeardown(async () => {
