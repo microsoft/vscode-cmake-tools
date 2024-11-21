d65b68540b34569f256fe3e3f52a9c20b06ede8c
Guillaume Peillex <guillaume.peillex@cea.fr>
2024-11-21 13:20:53 +0100

    First ry to retrieve CMakePresets name in the test

--- test/end-to-end-tests/single-root-ctest/test/ctest-run-tests.test.ts
+++ test/end-to-end-tests/single-root-ctest/test/ctest-run-tests.test.ts
@@ -9,8 +9,35 @@ import paths from '@cmt/paths';
 
 suite('Ctest run tests', () => {
     let testEnv: DefaultEnvironment;
+    const usedConfigPreset: string = "AllTestsSuccessfull";
+
+    async function getCMakePresetsAsJson() {
+        const preset_location: string = path.join(testEnv.projectFolder.location, "CMakePresets.json");
+        expect(fs.existsSync(preset_location)).to.eq(true, `CMakePresets.json file ${ preset_location } was not found`);
+        const content = fs.readFile(preset_location);
+        expect(content.toLocaleString()).to.not.eq('');
+        return JSON.parse(content.toString());
+    }
+
+    async function getSpecificPreset(presets_content: any, preset_name: string) {
+        expect(presets_content['configurePresets']).to.not.eq('', "Unable to find configurePresets section!");
+        const all_conf_presets = presets_content['configurePresets'];
+        for (let index = 0; index < all_conf_presets.length; index++) {
+            const conf_preset = all_conf_presets[index];
+            expect(conf_preset['name']).to.not.eq('', "Unable to find name of the current configure preset!");
+            if (conf_preset['name'] === preset_name) {
+                return conf_preset;
+            }
+        }
+        return undefined;
+    }
 
     async function cleanUpTestResultFiles() {
+        const used_preset = await getSpecificPreset(await getCMakePresetsAsJson(), usedConfigPreset);
+        expect(used_preset['cacheVariables']['TESTS_DIR']).to.not.eq('', "Unable to find the TESTS_DIR cache variable in the configure preset!");
+        expect(used_preset['cacheVariables']['TESTS_DIR'].split('/')[0]).to.eq(paths.tmpDir, "WARNING: The TEST_DIR variable does not seem to point to the temporary directory!");
+        const tests_dir_name = used_preset['cacheVariables']['TESTS_DIR'].split('/')[-1];
+        console.log("tests_dir_name is %s", tests_dir_name);
         const test_dir: string = path.join(paths.tmpDir, "vscode-cmake-tools-tests");
         await fs.rmdir(test_dir);
         const output_test_path: string = path.join(testEnv.projectFolder.location, testEnv.buildLocation, testEnv.executableResult);
@@ -32,7 +59,7 @@ suite('Ctest run tests', () => {
         await vscode.workspace.getConfiguration('cmake', vscode.workspace.workspaceFolders![0].uri).update('useCMakePresets', 'always');
         await vscode.commands.executeCommand('cmake.getSettingsChangePromise');
 
-        await vscode.commands.executeCommand('cmake.setConfigurePreset', 'AllTestsSuccessfull');
+        await vscode.commands.executeCommand('cmake.setConfigurePreset', usedConfigPreset);
         await vscode.commands.executeCommand('cmake.setBuildPreset', '__defaultBuildPreset__');
         await vscode.commands.executeCommand('cmake.setTestPreset', '__defaultTestPreset__');
         await vscode.commands.executeCommand('cmake.setPackagePreset', '__defaultPackagePreset__');
