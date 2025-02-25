b4fd96c7702fecf9979e30fd5fe11a970cd81224
Sean McManus <seanmcm@microsoft.com>
2025-02-25 14:44:52 -0800

    Fix linter/build error.

--- src/presets/preset.ts
+++ src/presets/preset.ts
@@ -11,7 +11,7 @@ import { errorHandlerHelper, expandString, ExpansionErrorHandler, ExpansionOptio
 import paths from '@cmt/paths';
 import { compareVersions, VSInstallation, vsInstallations, enumerateMsvcToolsets, varsForVSInstallation, getVcVarsBatScript } from '@cmt/installs/visualStudio';
 import { EnvironmentUtils, EnvironmentWithNull } from '@cmt/environmentVariables';
-import { defaultNumJobs, UseVsDeveloperEnvironment } from '@cmt/config';
+import { UseVsDeveloperEnvironment } from '@cmt/config';
 
 nls.config({ messageFormat: nls.MessageFormat.bundle, bundleFormat: nls.BundleFormat.standalone })();
 const localize: nls.LocalizeFunc = nls.loadMessageBundle();
