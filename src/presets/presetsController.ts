dafe3ae7e9182fed93440ab4eb79dda9ccf1ef15
Omotola <oakeredolu@microsoft.com>
2026-03-19 13:54:47 -0700

    switch the check of test preset compatibility to run regardless of configure preset check guard

--- src/presets/presetsController.ts
+++ src/presets/presetsController.ts
@@ -1194,11 +1194,11 @@ export class PresetsController implements vscode.Disposable {
             this._isChangingPresets = true;
         }
 
-        if (needToCheckConfigurePreset && presetName !== preset.defaultBuildPreset.name) {
+        if (presetName !== preset.defaultBuildPreset.name) {
             preset.expandConfigurePresetForPresets(this.folderPath, 'build');
             const _preset = preset.getPresetByName(preset.allBuildPresets(this.folderPath), presetName);
             const compatibility = this.checkCompatibility(this.project.configurePreset, _preset, this.project.testPreset, this.project.packagePreset, this.project.workflowPreset);
-            if (!compatibility.buildPresetCompatible) {
+            if (needToCheckConfigurePreset && !compatibility.buildPresetCompatible) {
                 log.warning(localize('build.preset.configure.preset.not.match', 'Build preset {0}: The configure preset does not match the active configure preset', presetName));
                 await vscode.window.withProgress(
                     {
@@ -1222,7 +1222,6 @@ export class PresetsController implements vscode.Disposable {
                     },
                     () => this.project.setTestPreset(null)
                 );
-                // Not sure we need to do the same for package/workflow build
             }
         }
         // Load the build preset into the backend
